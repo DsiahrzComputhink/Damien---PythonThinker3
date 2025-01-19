04c28e77c55d0bb7468fa4b59cf06ea3f604e956
DsiahrzComputhink <damiensiah11@gmail.com>
2025-01-19 03:13:38 +0000

    Jan 19, 2025, 3:13 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -10,7 +10,7 @@ for i in range(10):
 def bubblesort(numbers):
     n = len(numbers)
     for i in range(n):
-        for j in range(0,):
+        for j in range(0, n - i - 1):
             if numbers[j] > numbers[j+1]:
                 temp = numbers[j+1]
                 numbers[j+1] = numbers[j]
