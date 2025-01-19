8558b6928aa2f176339973b85464b04a042b6c56
DsiahrzComputhink <damiensiah11@gmail.com>
2025-01-19 03:42:13 +0000

    Jan 19, 2025, 3:42 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -1,8 +1,9 @@
 # DICTIONARY
-fruit = ["Apple","Apple","Banana"]
-cost = [1.50 , 2.55, 4.25]
+fruit = ["Apple","Orange","Banana"]
+costs = [1.50 , 2.55, 4.25]
 # Can be
-fruitcost
+fruitcost = {"Apple": 1.50, "Orange": 2.55, "Banana": 4.25}
+
 
 var = "var"
 print("This is a",var,"iable")
