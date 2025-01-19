efc117bfc8895d2f066e453982e0a6a2b3f029a5
DsiahrzComputhink <damiensiah11@gmail.com>
2025-01-19 03:49:28 +0000

    Jan 19, 2025, 3:49 AM

--- ARCHIVE/archive.py
+++ ARCHIVE/archive.py
@@ -1,18 +1,4 @@
 # TO TEST OUT
-# LESSON 1
-import random
-numbers = [5, 4, 3, 2, 1]
-
-def isascending(numbers):
-    for i in range(len(numbers) - 1):
-        if numbers[i] > numbers[i+1]:
-            return False
-    return True
-
-isascending(numbers)
-
-
-import random
 
 #------------------
 # LESSON 2
