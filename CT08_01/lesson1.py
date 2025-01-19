8947eb7af58e125ccf9ec6b5d2a8b18b855c12df
DsiahrzComputhink <damiensiah11@gmail.com>
2025-01-19 04:30:24 +0000

    Jan 19, 2025, 4:30 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -35,7 +35,10 @@ print(fruitcost.items())
 
 # silly challenge: make a fast food restruant
 
-menu = {"Cheeseburger":"$5.50","Fries":"$3.00","Milo":"$2.20",}
+menu = {
+    "Cheeseburger":"$5.50",
+    "Fries":"$3.00","Milo":"$2.20",
+    }
 
 def display_menu(menu):
     print("--------------------")
