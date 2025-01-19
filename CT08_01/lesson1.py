f586a3723ed35337cd10ca514025252b661b9896
DsiahrzComputhink <damiensiah11@gmail.com>
2025-01-19 03:15:22 +0000

    Jan 19, 2025, 3:15 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -14,7 +14,7 @@ def bubblesort(numbers):
     for i in range(n):
         for j in range(0, n - i - 1):
             if numbers[j] > numbers[j+1]:
-                numbers[j], numbers[j+1] = numbers[j+1], 
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
 
 print("Sorted List")
 print(bubblesort(numbers))
