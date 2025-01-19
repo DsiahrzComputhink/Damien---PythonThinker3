e58f5ebf55a135b4fc30d345f6cb36d6ee1b82d8
DsiahrzComputhink <damiensiah11@gmail.com>
2025-01-19 03:42:39 +0000

    Jan 19, 2025, 3:42 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -3,7 +3,7 @@ fruit = ["Apple","Orange","Banana"]
 costs = [1.50 , 2.55, 4.25]
 # Can be
 fruitcost = {"Apple": 1.50, "Orange": 2.55, "Banana": 4.25}
-# Key Val
+# Key Value is 
 
 var = "var"
 print("This is a",var,"iable")
