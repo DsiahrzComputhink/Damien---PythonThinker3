eb26ec45e0d041e36ae5709eb168b7220542fa7d
DsiahrzComputhink <damiensiah11@gmail.com>
2025-01-19 03:40:27 +0000

    Jan 19, 2025, 3:40 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -3,6 +3,6 @@ fruit = ["Apple","Apple","Banana"]
 cost = [1.50 , 2.55, 4.25]
 # Can be
 
-print("This is a sentence")
+print("This is a ",var,"iable")
 var = 10
-print
+print(f"this is a {var}iable")
