193695a7c9485ffd0ab628cebe4e01cdcd03c275
DsiahrzComputhink <damiensiah11@gmail.com>
2025-01-26 03:29:53 +0000

    Jan 26, 2025, 3:29 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -1,3 +1,40 @@
+# DICTIONARY
+fruit = ["Apple","Orange","Banana"]
+costs = [1.50 , 2.55, 4.25]
+# Can be
+fruitcost = {"Apple": 1.50, "Orange": 2.55, "Banana": 4.25}
+# Key Value is to the left
+# The right side is the value
+
+var = "var"
+print("This is a",var,"iable")
+print(f"this is a {var}iable")
+
+# Acessing a dictionary value
+print(fruitcost)
+
+print(f"The price of an orange is {fruitcost["Orange"]}")
+
+# Appending a new item to a dictionary
+fruitcost["Durian"] = 24.40
+print(fruitcost)
+
+# Deleting a item from the list
+del fruitcost["Apple"]
+print(f"Fruit Store:{fruitcost}")
+
+# Iterating through a dictionary
+for fruit in fruitcost:
+    price = fruitcost[fruit]
+    print(f"{fruit} costs {price}")
+
+# Method 2
+print(fruitcost.keys())
+print(fruitcost.values())
+print(fruitcost.items())
+
+# silly challenge: make a fast food restruant
+
 menu = {
     "Cheeseburger":"$5.50",
     "Fries":"$3.00",
@@ -16,7 +53,8 @@ def display_menu(menu):
     #-- ask what to order
     print("What would you like to order?")
     while True:
-        ordered = input("")
+        order = input("")
+        food
     
 
 
