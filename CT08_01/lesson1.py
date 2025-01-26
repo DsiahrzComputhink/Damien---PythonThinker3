f9fe2ae247d1792adc694827b303cb2b5531209b
DsiahrzComputhink <damiensiah11@gmail.com>
2025-01-26 03:54:25 +0000

    Jan 26, 2025, 3:54 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -53,9 +53,8 @@ def display_menu(menu, ordered):
     #-- ask what to order
     print("What would you like to order?")
     order = input("")
-    
+
     order = order.lower()
-    menu = menu.lower()
     if order in menu:
         print(f"{order} has been added to your order")
         ordered[order] = menu[order]
