480a879b384b0b9efeaecb05add7449ddd8f494e
DsiahrzComputhink <damiensiah11@gmail.com>
2025-01-26 04:10:14 +0000

    Jan 26, 2025, 4:10 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -82,6 +82,7 @@ def display_menu(menu, ordered):
             ordered[order] = menu[order]
         else:
             print(f"Sorry, we dont sell {order}")
+        print(itemordered)
         itemordered == 1
 
 
