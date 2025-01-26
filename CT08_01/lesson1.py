a1d802d30776dde19e04b0db58f0736f6f7d8002
DsiahrzComputhink <damiensiah11@gmail.com>
2025-01-26 03:51:01 +0000

    Jan 26, 2025, 3:51 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -36,28 +36,9 @@ print(fruitcost.items())
 # silly challenge: make a fast food restruant
 
 menu = {
-    "Cheeseburger":"$5.50",
-    "Fries":"$3.00",
-    "Milo":"$2.20",
+    "Cheeseburger":5.50,
+    "Fries":3.00,
+    "Milo":2.20,
     }
 
 ordered = {}
-
-def display_menu(menu):
-    print("---------------------------------")
-    print("{:^10}Welcome to Resturant{:^10}".format('*'*10, '*'*10,))
-    print("--------------Menu---------------")
-    for food, price in menu.items():
-        print("{:^30}:${:^8.2f}".format(food, price))
-    print("---------------------------------")
-    #-- ask what to order
-    print("What would you like to order?")
-    while True:
-        order = input("")
-        food
-    
-
-
-
-
-display_menu(menu)
