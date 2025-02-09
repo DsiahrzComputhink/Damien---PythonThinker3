e85fff142f0b34ba2678081f1019b5ca515d08dc
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-09 03:13:27 +0000

    Feb 9, 2025, 3:13 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -1,7 +1,7 @@
 class style():
-  RED = '\033[31m'
-  GREEN = '\033[32m'
-  BLUE = '\033[34m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+BLUE = '\033[34m'
 PURPLE = '\033[35m'
 RESET = '\033[0m'
 
