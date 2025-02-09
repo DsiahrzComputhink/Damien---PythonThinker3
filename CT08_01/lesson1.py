ee6e27ef0273a502bb51c24e8be6f11c252ae225
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-09 04:06:09 +0000

    Feb 9, 2025, 4:06 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -162,7 +162,7 @@ def display_menu(menu, ordered):
                     else:
                         print("Ok")
                 else:
-                    print("Alright, the price of the things you order")
+                    print("Alright, the price of")
             else:
                 print("Sorry, thats not a defined quantity.")
         else:
