1c221808e44f621aa965beb3560ade91163fa77d
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-09 03:58:16 +0000

    Feb 9, 2025, 3:58 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -152,7 +152,7 @@ def display_menu(menu, ordered):
             amt = input("")
             print()
             if amt.isnumeric():
-                print(f"{amt} {order}'s' has been added to your order")
+                print(f"style.bRED + {order} + style.RESET's' has been added to your order")
                 ordered[order] += int(amt)
             else:
                 print("Sorry, thats not a defined quantity.")
