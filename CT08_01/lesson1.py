b631ae223a38f7069b3a7fce51e2ca4c9df3ddf2
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-09 04:00:03 +0000

    Feb 9, 2025, 4:00 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -136,6 +136,7 @@ def display_menu(menu, ordered):
                     if total > 20:
                         print("Oh wait, your total is more than",style.bCYAN + "$20.00" + style.RESET,"!")
                         print("So your total will be")
+                        total = total 
                         print(style.bYELLOW + "${:^8.2f}".format(total) + style.RESET)
                         stop = 1
                     else:
