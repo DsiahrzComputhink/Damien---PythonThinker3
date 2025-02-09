9d5b29fea3bac16b0725fe534d2fb809f8d0e844
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-09 03:11:18 +0000

    Feb 9, 2025, 3:11 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -1,1 +1,12 @@
-print('\033[31mHello World but Red!\033[0m')
+os.system("")
+class style():
+  RED = '\033[31m'
+  GREEN = '\033[32m'
+  BLUE = '\033[34m'
+  RESET = '\033[0m'
+
+test1 = "ONE"
+test2 = "TWO"
+
+print(style.RED + "{0}".format(test1) + style.RESET)
+print(style.GREEN + "{0}".format(test2) + style.RESET)
