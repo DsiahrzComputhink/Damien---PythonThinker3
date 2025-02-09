d743958a49ea204da8101af7a47ce03ee2480f32
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-09 04:18:51 +0000

    Feb 9, 2025, 4:18 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -9,13 +9,13 @@ class style():
     d_cyan = '\033[36m'
     d_white = '\033[37m'
 
-    b_black = '\033[30m'
+    b_black = '\033[90m'
     b_red = '\033[31m'
     b_green = '\033[32m'
     b_yellow = '\033[33m'
     b_blue = '\033[34m'
-    b = '\033[35m'
-    break_cyan = '\033[36m'
+    b_purple = '\033[35m'
+    b_cyan = '\033[36m'
     b_white = '\033[37m'
 
     RESET = '\033[0m'
