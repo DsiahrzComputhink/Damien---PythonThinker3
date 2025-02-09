d86914ce80afad2ffc87057133ddf01450db5d2a
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-09 03:34:17 +0000

    Feb 9, 2025, 3:34 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -84,7 +84,7 @@ def display_menu(menu, ordered):
     for food, price in menu.items():
         print(style.bBLUE + "{:30}:     ${:^8.2f}".format(food, price) + style.RESET)
 
-    print(style.bCYAN + "For any purchase " + style.RESET)
+    print(style.bCYAN + "For any purchase more than $20.00,")
     print("-------------------------------------------")
     print("Type in 'Thats all' to stop ordering.")
 
