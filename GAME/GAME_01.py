cc7e6961680729bd8ffdaa8084e7fb738e3708a3
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-09 13:52:17 +0000

    Feb 9, 2025, 1:52 PM

--- GAME/GAME_01.py
+++ GAME/GAME_01.py
@@ -52,7 +52,7 @@ menu_text = f"""
 {style.bgray}--------------------Cmds--------------------{style.RESET}
 
 {style.bblue}bruteforce.password{style.RESET} . {style.bblue}[CRACK PASSWORD]{style.RESET}
-{style.bblue}menu{style.RESET} . {style.bblue}[CRACK PASSWORD]{style.RESET}
+{style.bblue}menu{style.RESET} . {style.bblue}[SHOW MENU]{style.RESET}
 
 {style.bgray}--------------------------------------------{style.RESET}
 """
