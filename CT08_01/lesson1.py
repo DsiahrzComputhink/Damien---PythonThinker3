a553a669919e65234ec8823c9a5795b9c85ae6d0
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-16 04:09:03 +0000

    Feb 16, 2025, 4:09 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -106,8 +106,10 @@ def attendance_percentage(student: str, students: dict) -> float:
         TRUE = attendancelist.count(True)
         FALSE = attendancelist.count(False)
         Denom = len(attendancelist)
-
-        print(round(TRUE/Denom) * 100,2)
+        p = round((TRUE/Denom) * 100,2)
+        a = round((FALSE/Denom) * 100,2)
+        print("Present",style.bgreen + f"{p}" + style.RESET,"of the time")
+        print("Absent",style.bgreen + f"{a}" + style.RESET,"of the time")
     else:
         print(style.dred + "STUDENT DOES NOT EXIST" + style.RESET)
         
