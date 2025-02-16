6f63943a15ff6f9abc476860031f8e46bc96737b
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-16 04:03:08 +0000

    Feb 16, 2025, 4:03 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -100,7 +100,7 @@ def attendance_percentage(student: str, students: dict) -> float:
         #         print(style.bgreen + "#" + style.RESET)
         #     if attendancelist(i-1) == False:
         #         print(style.bred + "#" + style.RESET)
-    
+
 
         return round((TRUE/Denom) * 100,2)
     else:
