bc26c52943a50aff8de487016db380e1c02e0689
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-16 04:04:22 +0000

    Feb 16, 2025, 4:04 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -86,6 +86,18 @@ def take_attendance(students: dict) -> dict:
 
 
 def attendance_percentage(student: str, students: dict) -> float:
+    if student == "all":
+        if student in students:
+            attendancelist = students[student]
+            TRUE = attendancelist.count(True)
+            FALSE = attendancelist.count(False)
+            Denom = len(attendancelist)
+
+            return round((TRUE/Denom) * 100,2)
+        else:
+            print(style.dred + "STUDENT DOES NOT EXIST" + style.RESET)
+
+        return 0.0
     if student in students:
 
         attendancelist = students[student]
