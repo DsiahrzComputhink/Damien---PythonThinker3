21cb4dec518c8d3d1d2f7eedbd6ea1a19db16017
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-16 03:05:41 +0000

    Feb 16, 2025, 3:05 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -29,17 +29,3 @@ class style():
 
     RESET = '\033[0m'
 
-test1 = "ONE"
-test2 = "TWO"
-
-print(style.bgray + "Placeholder" + style.RESET)
-
-
-def calculate_area(length: int | float,width: int | float):
-    return length*width
-
-def caculate_peri(length, width):
-    return 2 * length*width
-
-length = 6
-width = 4
