a79f44e46afe32fa98158f9d2850d9a70eeb5f49
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-16 04:14:35 +0000

    Feb 16, 2025, 4:14 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -125,5 +125,5 @@ def notify(students: dict, threshold: float) -> list:
     for student in students():
         percentage = attendance_percentage(student, students)
         if percentage < threshold:
-            print()
+            print(style.bred + )
 
