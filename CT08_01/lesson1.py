41de1b3f437321fb7ebaf38bc8e98706f3cfab66
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-16 03:25:09 +0000

    Feb 16, 2025, 3:25 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -65,7 +65,7 @@ def createnewstudent():
 def take_attendance(students: dict) -> dict:
     for student in students:
         print(student)
-        print("Is",style.bblue + f"{student}" + style.RESET,"Present?",style.dgreen + "y" + style.RESET,"/",style.bblue + f"{student}" + style.RESET)
+        print("Is",style.bblue + f"{student}" + style.RESET,"Present?",style.dgreen + "y" + style.RESET,"/",style.dred + "n" + style.RESET)
     return {}
 
 take_attendance(students)
