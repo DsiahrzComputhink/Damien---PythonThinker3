7ff11b1d9b2309ed45341b25b3146dca29170e71
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-16 03:26:25 +0000

    Feb 16, 2025, 3:26 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -67,7 +67,7 @@ def take_attendance(students: dict) -> dict:
         print(student)
         print("Is",style.bblue + f"{student}" + style.RESET,"Present?",style.dgreen + "Y" + style.RESET,"/",style.dred + "N" + style.RESET)
         attendance = input()
-        attendance.lower()
+        (student).lower()
         
 
     return {}
