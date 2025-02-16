c4686f97d25b236bd363c178504406aadcab235a
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-16 09:42:06 +0000

    Feb 16, 2025, 9:42 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -89,17 +89,16 @@ def attendance_percentage(student: str, students: dict) -> float:
     if student == "all":
         for student in students:
             print(student)
-            if student in students:
-                attendancelist = students[student]
-                TRUE = attendancelist.count(True)
-                FALSE = attendancelist.count(False)
-                Denom = len(attendancelist)
-                p = round((TRUE/Denom) * 100,2)
-                a = round((FALSE/Denom) * 100,2)
-                print(style.bblue + f"{student}" + style.RESET,"is")
-                print("Present",style.bgreen + f"{p}%" + style.RESET,"of the time")
-                print("Absent",style.bgreen + f"{a}%" + style.RESET,"of the time")
-                return round((TRUE/Denom) * 100,2)
+            attendancelist = students[student]
+            TRUE = attendancelist.count(True)
+            FALSE = attendancelist.count(False)
+            Denom = len(attendancelist)
+            p = round((TRUE/Denom) * 100,2)
+            a = round((FALSE/Denom) * 100,2)
+            print(style.bblue + f"{student}" + style.RESET,"is")
+            print("Present",style.bgreen + f"{p}%" + style.RESET,"of the time")
+            print("Absent",style.bgreen + f"{a}%" + style.RESET,"of the time")
+            return round((TRUE/Denom) * 100,2)
         else:
             print(style.dred + "STUDENT DOES NOT EXIST" + style.RESET)
         return 0.0
