68478e966f212b5e64474d1d00f25aa71b4042ff
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-16 12:53:58 +0000

    Feb 16, 2025, 12:53 PM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -44,7 +44,7 @@ def createnewstudent(num, num_attendance=5):
     for i in range(num):
         attendance_list = []
         for _ in range(num_attendance):
-            attendance_list.append(random.choice([True, True, False, False]))
+            attendance_list.append(random.choice([True, True, True, False]))
         
         students[f"TEST{NAME}"] = attendance_list
         NAME += 1
