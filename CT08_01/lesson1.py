b57296197c9b73e0aedbcf142d847b05253570e8
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-16 10:01:10 +0000

    Feb 16, 2025, 10:01 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -171,6 +171,6 @@ def notify(students: dict, threshold: float) -> list:
     else:
         print(style.dgreen + "No students below threshold." + style.RESET)
     
-createnewstudent(1000)
+createnewstudent(10000)
 attendance_percentage("all",students)
 notify(students,50.0)
