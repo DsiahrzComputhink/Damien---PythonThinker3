297fb7b811172b6deedf3a18600d0f5ee6abb527
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-18 00:38:42 +0000

    Feb 18, 2025, 12:38 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -171,4 +171,4 @@ while True:
     print(style.bblue + "1" + style.RESET, "Add new student")
     print(style.bblue + "2" + style.RESET, "Take attendance")
     print(style.bblue + "3" + style.RESET, "Check attendance percentage")
-    print(style.bblue + "4" + style.RESET, "Check attende")
+    print(style.bblue + "4" + style.RESET, "Notify people with low attendance percentage")
