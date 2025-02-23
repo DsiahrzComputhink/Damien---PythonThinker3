d511f1ee08d5a4897c2fc93d0a13976aef68b936
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-23 03:19:21 +0000

    Feb 23, 2025, 3:19 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -203,7 +203,8 @@ def commandinput():
         elif command == "5":
             print("Closing all instances")
             time.sleep(1)
-            print(style.bcyan + "Program Closed." + style.RESET, "Goodbye!")
+            print(style.bcyan + "Program Closed." + style.RESET)
+            print("Goodbye!")
             stop += 1
         else:
             print(style.bred + "ERROR" + style.RESET)
