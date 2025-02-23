8945e79d355b551463db9a17c8c7c3cced9a3510
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-23 03:46:36 +0000

    Feb 23, 2025, 3:46 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -57,7 +57,7 @@ student_answers = {
 def gradeallstudents(student_answers, answer_key):
     for student in student_answers:
         for answers in student:
-            if answers == answer_key[int(answers)]:
+            if answers == answer_key:
                 print(answers)
                 print(answer_key)
                 print("yes")
