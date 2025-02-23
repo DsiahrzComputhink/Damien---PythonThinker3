2326e01edcd30ba90bbf79809c28f10866498dbe
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-23 04:24:14 +0000

    Feb 23, 2025, 4:24 AM

--- GAME/GAME_03.py
+++ GAME/GAME_03.py
@@ -32,6 +32,7 @@ def bubblesort(numbers):
         for j in range(0, n - i - 1):
             if numbers[j] > numbers[j + 1]:
                 numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+                print(numbers)
     return numbers
 
 print("Sorted List")
