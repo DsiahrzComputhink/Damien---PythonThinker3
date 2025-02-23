574af456df5b4b95cd59aca30c3a3ee11fca916b
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-23 04:24:05 +0000

    Feb 23, 2025, 4:24 AM

--- GAME/GAME_03.py
+++ GAME/GAME_03.py
@@ -1,1 +1,38 @@
 # am bored
+
+# LESSON 1
+import random
+numbers = [5, 4, 3, 2, 1]
+
+def isascending(numbers):
+    for i in range(len(numbers) - 1):
+        if numbers[i] > numbers[i+1]:
+            return False
+    return True
+
+isascending(numbers)
+
+
+import random
+
+# LESSON 2
+# ALGORITHMS
+numbers = []
+
+for i in range(1000):
+    x = random.randint(1,1000)
+    numbers.append(x)
+
+print("Unsorted List")
+print(numbers)
+
+def bubblesort(numbers):
+    n = len(numbers)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+    return numbers
+
+print("Sorted List")
+print(bubblesort(numbers))
