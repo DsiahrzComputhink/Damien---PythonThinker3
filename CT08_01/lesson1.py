39513f81d6cec799b85d91a728de4abf15f45b92
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-23 04:01:25 +0000

    Feb 23, 2025, 4:01 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -63,9 +63,9 @@ def gradeallstudents(student_answers: dict, answer_key: list) -> dict:
             for i in range(len(answer_key)):
                 if answers[i] == answer_key[i]:
                     score += 1
-    print(student)
-    quizscores[f"{student}"] = score
-    print(quizscores)
+            print(student)
+            quizscores[f"{student}"] = score
+            print(quizscores)
         
 
 
