78652c96cf3bf7c935f7f999443f046662fa02dd
DsiahrzComputhink <damiensiah11@gmail.com>
2025-02-23 03:42:22 +0000

    Feb 23, 2025, 3:42 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -57,7 +57,7 @@ student_answers = {
 def gradeallstudents(student_answers, answer_key):
     for student in student_answers:
         for answers in answer_key:
-            students
+            student_answers[student]
         print(student)
 
 
