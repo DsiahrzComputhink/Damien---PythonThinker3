c325da9328c343642e0436baf9ac9ca1d65d0aa5
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-02 03:58:06 +0000

    Mar 2, 2025, 3:58 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -23,7 +23,7 @@
 import os
 filepath = os.getcwd()
 
-fullpath = os.path.join(filepath,"ARCHIVE","example.txt")
+fullpath = os.path.join(filepath,"ARCHIVE","L07-File_Input\Output [example.txt]")
 
 if os.path.exists(fullpath):
     print("{} exist".format(fullpath))
