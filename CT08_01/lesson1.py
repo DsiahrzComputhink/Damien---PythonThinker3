753ca69c42c5c8cea210a65dfed08951b0b12e7d
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-02 12:04:42 +0000

    Mar 2, 2025, 12:04 PM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -66,8 +66,8 @@ with open(textfile, "w") as file:
     file.writelines(lines)
 
 num = 0
-for i in range(10):
-    num += 1
+for i in range(9):
+    num += 0
     print(num)
     with open(textfile, "a") as file:
         file.write(f"{num}  ")
