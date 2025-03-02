04350e0d0279070ade65b4f7dfdbabbc81d2c83f
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-02 03:06:02 +0000

    Mar 2, 2025, 3:06 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -90,7 +90,8 @@ def findhighestscorer(quizscores: dict) -> float:
 def displayresults(quizscores: dict) -> float:
     print("Hi")
     for student, score in quizscores.items():
-        print(student,)
+        student
+        print(f"{student}")
 
 quizscores = gradeallstudents(student_answers,answer_key)
 calculateclassaverage(quizscores)
