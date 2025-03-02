33d255cb15253032ab564030b118bdde9083dff2
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-02 03:50:36 +0000

    Mar 2, 2025, 3:50 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -1,23 +1,24 @@
-# File Input / Output 
-# File Modes:
-# r: Read Modes - Read the data of a file
-# w: Write Modes - Writes data to a file
-# a: Append Modes - Adds data to a file
+# File Input / Output
 
-# File Operation: 
-# open() - Establishes a connection to a file
-# read/write/append - Processes File connection
-# close: - Frees resources after file operations
+    # File Modes:
+        # r: Read Modes - Read the data of a file
+        # w: Write Modes - Writes data to a file
+        # a: Append Modes - Adds data to a file
 
-# Advantages:
-# Persistent Storage - Retains data after the program ends
-# Flexibility - Handle various file formats (text, binary, CSV, etc)
-# Scalability - Manage large datasets efficently
+    # File Operation: 
+        # open() - Establishes a connection to a file
+        # read/write/append - Processes File connection
+        # close: - Frees resources after file operations
 
-# Disadvantages:
-# Data Security - Avoid unauthorized access
-# Handling Errors - Deal with missing files or unexpected Disadvantages
-# Performance - Not optimized for large file operations
+    # Advantages:
+    # Persistent Storage - Retains data after the program ends
+    # Flexibility - Handle various file formats (text, binary, CSV, etc)
+    # Scalability - Manage large datasets efficently
+
+    # Disadvantages:
+    # Data Security - Avoid unauthorized access
+    # Handling Errors - Deal with missing files or unexpected Disadvantages
+    # Performance - Not optimized for large file operations
 
 import os
 filepath = os.getcwd()
