53aa683c84d896b6bfc2566f62c96201ddfea8b3
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-03 09:42:40 +0000

    Mar 3, 2025, 9:42 AM

--- Miscellaneous/GAME_03.py
+++ Miscellaneous/GAME_03.py
@@ -42,7 +42,7 @@ else:
 
 
 # Testing if dictionary works within a text file
-DefaultMemory = {"Lives": 0, "Correct": 0, "Wrong": 0, "Round":0}
+DefaultMemory = {"Lives":0, "Correct":0, "Wrong":0, "Round":0}
 Memory = {}
 
 
