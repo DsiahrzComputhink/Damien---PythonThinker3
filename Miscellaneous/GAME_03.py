b2dc8c8552800873c1d15abd16b41835ff48cce5
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-03 08:31:16 +0000

    Mar 3, 2025, 8:31 AM

--- Miscellaneous/GAME_03.py
+++ Miscellaneous/GAME_03.py
@@ -21,12 +21,12 @@ with open(textfile, "r") as file:
 
 
 try:
-    print(2/0)
+    content = dict
 except SyntaxError:
     print("You can't divide by zero!")
 
 print(content)
-"content" = dict
+
 if content is dict:
     print("Content is Dictionary")
 else:
