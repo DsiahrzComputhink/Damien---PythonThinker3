949475fe8229d55ce1a7900902ecb1dd24ad45fc
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-04 08:37:09 +0000

    Mar 4, 2025, 8:37 AM

--- Miscellaneous/GAME_03.py
+++ Miscellaneous/GAME_03.py
@@ -238,7 +238,7 @@ words = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         ]
 
-word = i
+word = random.randint()
 from sympy import Symbol
 
 x = Symbol('x')
