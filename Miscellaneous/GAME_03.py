725b3c50825ab7e1b1fbff6dd378fbc865e1efa8
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-04 01:08:02 +0000

    Mar 4, 2025, 1:08 AM

--- Miscellaneous/GAME_03.py
+++ Miscellaneous/GAME_03.py
@@ -50,7 +50,7 @@ with open(textfile, "r") as file:
         content = file.read()
 memory = eval(content)
 allow = 0
-if str(memory) == str(content):
+if str(memory) == str("hi"):
 
     if "Lives" in memory.keys():
         allow = 1
