b5ebae7d21a25596842d275d705331c12927e854
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-04 08:38:06 +0000

    Mar 4, 2025, 8:38 AM

--- Miscellaneous/GAME_03.py
+++ Miscellaneous/GAME_03.py
@@ -233,16 +233,3 @@ def whiteboard():
 # CheckData()
 # SaveData()
  # !pip3 install SymPy
-
-words = [
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        ]
-
-word = random.randint(0,len(words)-1)
-print(word)
-from sympy import Symbol
-
-x = Symbol('x')
-
-print(7*x + x + 1)
-# 2*x + 1
