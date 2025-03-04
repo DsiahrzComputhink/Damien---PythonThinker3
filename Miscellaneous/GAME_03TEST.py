cf543e7310627554cb78e0b85ba083c38d758c54
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-04 10:05:00 +0000

    Mar 4, 2025, 10:05 AM

--- Miscellaneous/GAME_03TEST.py
+++ Miscellaneous/GAME_03TEST.py
@@ -1,6 +1,8 @@
 import random
 from sympy import Symbol
 from sympy import *
+min = 0
+max = 10
 
 words = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
