d6e88ce3f261be8b94787520aa5260e4460c8938
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-04 08:36:54 +0000

    Mar 4, 2025, 8:36 AM

--- Miscellaneous/GAME_03.py
+++ Miscellaneous/GAME_03.py
@@ -238,6 +238,7 @@ words = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         ]
 
+word = i
 from sympy import Symbol
 
 x = Symbol('x')
