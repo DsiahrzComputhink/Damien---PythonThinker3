5a7d013dfa7aa7cfe6663a9d376aeb8e3efca16e
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-04 08:40:48 +0000

    Mar 4, 2025, 8:40 AM

--- Miscellaneous/GAME_03TEST.py
+++ Miscellaneous/GAME_03TEST.py
@@ -3,12 +3,13 @@ import random
 words = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         ]
-
 wordnumber = random.randint(0,len(words)-1)
+def generatealgebra(word):
 
-from sympy import Symbol
 
-x = Symbol(words[wordnumber])
+    
 
-print(7*x + x + 1)
-# 2*x + 1
+    x = Symbol(words[wordnumber])
+
+    print(7*x + x + 1)
+    # 2*x + 1
