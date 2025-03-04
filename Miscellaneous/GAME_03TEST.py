de7edf3643d49c9e2a872cfe5218ab9dc4a25a83
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-04 08:53:06 +0000

    Mar 4, 2025, 8:53 AM

--- Miscellaneous/GAME_03TEST.py
+++ Miscellaneous/GAME_03TEST.py
@@ -4,17 +4,18 @@ from sympy import Symbol
 words = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         ]
-wordnumber = random.randint(0,len(words)-1)
+
 
 def generatealgebra(word):
+    
     wordnumber = random.randint(0,len(words)-1)
 
 
     symbol = Symbol(words[int(wordnumber)])
     return symbol
 
-x = generatealgebra(wordnumber)
-y = generatealgebra(wordnumber)
+x = generatealgebra(words)
+y = generatealgebra(words)
 
 print(7*x + y + 1)
 # 2*x + 1
