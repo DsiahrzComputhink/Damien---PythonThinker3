30dc1f0e558f836a12a441acc9b23300f908cf3d
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-04 08:35:33 +0000

    Mar 4, 2025, 8:35 AM

--- Miscellaneous/GAME_03.py
+++ Miscellaneous/GAME_03.py
@@ -237,5 +237,8 @@ def whiteboard():
 from sympy import Symbol
 
 x = Symbol('x')
+words = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    ]
 print(x + x + 1)
 # 2*x + 1
