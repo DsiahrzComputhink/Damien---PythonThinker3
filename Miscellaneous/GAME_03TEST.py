d7db547da4d66136699a8f37ecdb83e0c310212c
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-04 10:26:24 +0000

    Mar 4, 2025, 10:26 AM

--- Miscellaneous/GAME_03TEST.py
+++ Miscellaneous/GAME_03TEST.py
@@ -77,6 +77,6 @@ def solveSimul():
     print(LINE)
     print(style.BOLD + "Simultaneous Equation" + style.RESET)
     print("Solve the 2 equations.")
-    print(f"()")
+    print(f"({x})")
 
 solveSimul()
