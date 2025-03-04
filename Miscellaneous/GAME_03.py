56c04c0a5a71ee8d38d62b3339ddc0996ff320e9
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-04 08:21:04 +0000

    Mar 4, 2025, 8:21 AM

--- Miscellaneous/GAME_03.py
+++ Miscellaneous/GAME_03.py
@@ -211,6 +211,11 @@ line_width_slider = tk.Scale(controls_frame, from_=1, to=10, orient="horizontal"
 line_width_slider.set(line_width)
 line_width_slider.pack(side="left", padx=5, pady=5)
 
+canvas.bind("<Button-1>", start_drawing)
+canvas.bind("<B1-Motion>", draw)
+canvas.bind("<ButtonRelease-1>", stop_drawing)
+
+root.mainloop()
 # This is super important.
 # CheckData()
 # SaveData()
