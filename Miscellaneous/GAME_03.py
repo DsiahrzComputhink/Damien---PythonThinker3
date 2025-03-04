72a8e3662961226cd13e19eb0b6185e64c49073b
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-04 08:25:58 +0000

    Mar 4, 2025, 8:25 AM

--- Miscellaneous/GAME_03.py
+++ Miscellaneous/GAME_03.py
@@ -226,6 +226,7 @@ text_widget = tk.Text(controls_frame, height=6, width=120)
 text_widget.pack(side="left", padx=5, pady=5)
 
 root.mainloop()
+print(style.bblue + "Whiteboard is ready.")
 # This is super important.
 # CheckData()
 # SaveData()
