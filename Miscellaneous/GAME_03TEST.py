9ff5753ddc1a4ea77b5eab229efee0bde49fc0ac
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-09 04:26:24 +0000

    Mar 9, 2025, 4:26 AM

--- Miscellaneous/GAME_03TEST.py
+++ Miscellaneous/GAME_03TEST.py
@@ -87,6 +87,7 @@ def solveSimul(x,y,xval,yval,num1,num2,num3,num4):
             return False
         elif xvalans == xval:
             if yvalans != yval:
+                print("YVAL IS FALSE")
                 return False
             elif yvalans == yval:
                 return True
