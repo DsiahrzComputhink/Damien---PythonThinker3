7795e456bb1fa4fcb62251c537a2a6773af3578b
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-09 03:15:43 +0000

    Mar 9, 2025, 3:15 AM

--- CT08_01/midsemesterpractical.py
+++ CT08_01/midsemesterpractical.py
@@ -3,7 +3,9 @@ n = len(my_list)
 
 # For i in range,
 for i in range(n+1):
+    # For each number in the list,
     for j in range(0, n-1):
+        # If 
         if my_list[j] > my_list[j+1]:
             my_list[j], my_list[j+1] = my_list[j+1], my_list[j]
 
