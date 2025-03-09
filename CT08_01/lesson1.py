008b91f8666ee057c96b42b47016ade2af20f7f3
DsiahrzComputhink <damiensiah11@gmail.com>
2025-03-09 03:52:21 +0000

    Mar 9, 2025, 3:52 AM

--- CT08_01/lesson1.py
+++ CT08_01/lesson1.py
@@ -104,20 +104,3 @@ else:
 #     content[f"{food}"] = price
 
 # print(content)
-
-# DICTIONARY
-fruit = ["Apple","Orange","Banana"]
-costs = [1.50 , 2.55, 4.25]
-# Can be
-fruitcost = {"Apple": "haha", "Orange": 2.55, "Banana": 4.25}
-# Key Value is to the left
-# The right side is the value
-
-var = "var"
-print("This is a",var,"iable")
-print(f"this is a {var}iable")
-
-# Acessing a dictionary value
-print(fruitcost)
-
-print(f"The price of an orange is {fruitcost["Apple"]}")
