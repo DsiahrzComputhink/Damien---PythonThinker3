426f9bd15c13ab1f0181a8cb8c9ce2a005b5eaea
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-06 03:13:42 +0000

    Apr 6, 2025, 3:13 AM

--- CT08/lesson1.py
+++ CT08/lesson1.py
@@ -58,7 +58,7 @@ def removepunctuation(encrypted):
     for char in encrypted:
         if char in punctuation:
             print(char)
-            char.replace(",","")
+            encrypted.replace(",","")
             
     print(style.bgreen + "WITHOUT PUNCTUATION" + style.RESET)
     print(encrypted)
