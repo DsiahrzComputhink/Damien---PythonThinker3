f29818ef07ce638a0cb0447838a138ae9f22d7b0
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-13 03:27:20 +0000

    Apr 13, 2025, 3:27 AM

--- CT08/lesson1.py
+++ CT08/lesson1.py
@@ -78,7 +78,7 @@ def createusername(username: str):
         password = generatepassword()
         print(password)
         userDatabase[f"{username}"] = {}
-        userDatabase[f"{username}"]['Z'] = 5
+        userDatabase[f"{username}"]['USEDPASSWORDS'] = {}
         userDatabase[f"{username}"]['PASSWORD'] = password
         print(userDatabase)
     return username
