acc8643fc8e3c5f464280bdd1789c8df610adf48
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-13 04:05:49 +0000

    Apr 13, 2025, 4:05 AM

--- CT08/lesson1.py
+++ CT08/lesson1.py
@@ -146,7 +146,7 @@ def login():
 def viewdatabase(userdb: dict) -> None:
     for user in userdb:
         print(style.bcyan + f"{user}:" + style.RESET,"(")
-        print()
+        print("    ")
 
 createusername("username")
 # updatepassword("username","password")
