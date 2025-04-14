63fcbfc5086fbc1d6230ccb86f52576babdc7768
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-14 11:54:12 +0000

    Apr 14, 2025, 11:54 AM

--- CT08 Python/Miscellaneous/GAME_03.py
+++ CT08 Python/Miscellaneous/GAME_03.py
@@ -46,50 +46,6 @@ LINE = style.bgray + "------------------------------" + style.RESET
 DefaultMemory = {"Lives":0, "Correct":0, "Wrong":0, "Round":0, "Difficulty":0, "Money":0}
 memory = {}
 
-with open(textfile, "r") as file:
-        content = file.read()
-memory = eval(content)
-allow = 0
-if str(memory) == str(content):
-    if "Lives" in memory.keys():
-        allow = 1
-    else:
-        allow = 0
-    if allow == 1:
-        if "Correct" in memory.keys():
-            allow = 1
-        else:
-            allow = 0
-        if allow == 1:
-            if "Wrong" in memory.keys():
-                allow = 1
-            else:
-                allow = 0
-            if allow == 1:
-                if "Round" in memory.keys():
-                    allow = 1
-                else:
-                    allow = 0
-                if allow == 1:
-                    if "Difficulty" in memory.keys():
-                        allow = 1
-                    else:
-                        allow = 0
-                    if allow == 1:
-                        if "Money" in memory.keys():
-                            allow = 1
-                        else:
-                            allow = 0
-if allow == 0:
-    print(LINE)
-    print(style.bred + "Your memory was corrupted." + style.RESET)
-    print(style.bblue + "We have reset your memory for you." + style.RESET)
-    print(LINE)
-    with open(textfile, "w") as file:
-        file.write(str(DefaultMemory))
-    with open(textfile, "r") as file:
-        content = file.read()
-        print(content)
 
 
 def CheckData():
