dd48f2c33c68e0d6f48a423bad257c02b1dc3624
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-14 12:02:20 +0000

    Apr 14, 2025, 12:02 PM

--- CT08 Python/Miscellaneous/GAME_03TEST.py
+++ CT08 Python/Miscellaneous/GAME_03TEST.py
@@ -76,7 +76,7 @@ def solveSimul(x,y,xval,yval,num1,num2,num3,num4):
     print(style.bcyan + "Solve the 2 equations." + style.RESET)
     print(f"({(num1*x + num2*y)} = {equ1})")
     print(f"({(num3*x + num4*y)} = {equ2})")
-    print(style.bgray + "The letters go alphabetically, please be aware about it.")
+    print(style.bgray + "The letters are not sorted out alphabetically, please be aware about it.")
     xvalans = input(style.bpurple + f"{x} = " + style.RESET)
     yvalans = input(style.bpurple + f"{y} = " + style.RESET)
     if str(xvalans) != str(xval):
