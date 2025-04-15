3af761be2c908137b083360cd0d448f17f7d7621
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-15 07:54:25 +0000

    Apr 15, 2025, 7:54 AM

--- CT08 Python/Miscellaneous/GAME_03[TESTING FUNCTIONS].py
+++ CT08 Python/Miscellaneous/GAME_03[TESTING FUNCTIONS].py
@@ -171,6 +171,7 @@ def AlgebraicFactorization(x,y):
         print(style.bgray + "The question is asking for the coefficent" + style.RESET)
         xvalans = input(style.bpurple + f"{x} = " + style.RESET)
         yvalans = input(style.bpurple + f"{y} = " + style.RESET)
+        turnnumberintopositive(xvalans)
         print(xvalans, xval)
         print(yvalans, yval)
         if str(xvalans) != str(xval):
