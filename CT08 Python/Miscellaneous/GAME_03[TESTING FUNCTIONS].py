59c76041e8b9e0422397d236d3d7fe92b1fee8e9
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-15 07:51:17 +0000

    Apr 15, 2025, 7:51 AM

--- CT08 Python/Miscellaneous/GAME_03[TESTING FUNCTIONS].py
+++ CT08 Python/Miscellaneous/GAME_03[TESTING FUNCTIONS].py
@@ -158,7 +158,7 @@ def AlgebraicFactorization(x,y):
     print(style.BOLD + "Algebraic Factorization" + style.RESET)
     print(LINE)
     print(style.bcyan + "Simplify this Equation" + style.RESET)
-    print(style.bgray + "The question is asking for the coefficent" + style.RESET)
+    print(style.bgray + "Factorize the Equation first, and then find the coefficent of whatever that is being s" + style.RESET)
     difficulty = random.randint(1,3)
     if difficulty == 1:
         num1 = generaterandomnumber(min,max)
