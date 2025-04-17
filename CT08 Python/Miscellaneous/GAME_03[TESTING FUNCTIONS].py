947c9f841439480d64b15269b97571926e4bb4e5
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-17 13:39:47 +0000

    Apr 17, 2025, 1:39 PM

--- CT08 Python/Miscellaneous/GAME_03[TESTING FUNCTIONS].py
+++ CT08 Python/Miscellaneous/GAME_03[TESTING FUNCTIONS].py
@@ -226,11 +226,8 @@ def AlgebraicFactorization(x,y):
                 return True
             
 def QuadraticExpansion(x,R1,R2,axval,bxval):
-    # x = symbol
-    # R1 and R2 are integers, axval and bxval are coefficents
     ax = x*axval
     bx = x*bxval
-    # (ax,R1)(bx,R2) = ax*bx + (ax*R2 + bx*R1) + R1*R2
     a = ax*bx
     b = ax*R2 + bx*R1
     c = R1*R2
