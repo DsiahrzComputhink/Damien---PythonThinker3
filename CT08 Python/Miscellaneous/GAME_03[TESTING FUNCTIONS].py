47c7a630257a0a4d5b04bbf94f7958dec241167c
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-17 13:38:46 +0000

    Apr 17, 2025, 1:38 PM

--- CT08 Python/Miscellaneous/GAME_03[TESTING FUNCTIONS].py
+++ CT08 Python/Miscellaneous/GAME_03[TESTING FUNCTIONS].py
@@ -237,7 +237,7 @@ def QuadraticExpansion(x,R1,R2,axval,bxval):
     print(latex(a + b + c))
     abc = factor(a + b + c)
 
-print()
+print(math.sqrt(-1))
 
 x = generatealgebra(words)
 y = generatealgebra(words)
