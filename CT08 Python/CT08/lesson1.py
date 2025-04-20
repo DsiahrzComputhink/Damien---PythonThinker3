eed7cd370594ec10dc4ac29109524e87d2210fe0
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-20 03:48:09 +0000

    Apr 20, 2025, 3:48 AM

--- CT08 Python/CT08/lesson1.py
+++ CT08 Python/CT08/lesson1.py
@@ -31,12 +31,18 @@ class style():
 
 LINE = style.bgray + "------------------------------" + style.RESET
 
-import os
-FilePath = os.getcwd()
-# textfile = os.path.join(FilePath,"ARCHIVE","L07-File_Input\Output [example].txt")
 
-# if os.path.exists(textfile):
-#    print("hi")
+import os
+
+
+FilePath = os.getcwd()
+textfile = os.path.join(FilePath,"ARCHIVE","L07-File_Input\Output [example].txt")
+
+if os.path.exists(textfile):
+    print(style.bred + "{} exist".format(textfile) + style.bgreen)
+else:
+    print("{} Does not exist".format(textfile))
+
 
     # character functions
 def encryptChar(char: str, num: int):
