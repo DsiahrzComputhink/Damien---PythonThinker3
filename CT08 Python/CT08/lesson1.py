c6048f354ff4027159d019e793b6c155c6fbf5d6
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-21 05:09:26 +0000

    Apr 21, 2025, 5:09 AM

--- CT08 Python/CT08/lesson1.py
+++ CT08 Python/CT08/lesson1.py
@@ -124,7 +124,7 @@ def console():
                 if number.isnumeric():
                     encrypted = encryptSentence(sentence,int(number))
                     print(LINE)
-                    print(style.bpurple + "")
+                    print(style.bpurple + "Decrypted Sentence: ")
                 else:
                     print(style.bred + "Number is not numeric")
         else:
