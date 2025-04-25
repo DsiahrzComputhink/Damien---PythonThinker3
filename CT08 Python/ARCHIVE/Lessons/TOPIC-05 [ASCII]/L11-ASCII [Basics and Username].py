95e97c6b0fedebeb4ed38c7287b62e16e9d49b85
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-25 23:22:00 +0000

    Apr 25, 2025, 11:22 PM

--- CT08 Python/ARCHIVE/Lessons/TOPIC-05 [ASCII]/L11-ASCII [Basics and Username].py
+++ CT08 Python/ARCHIVE/Lessons/TOPIC-05 [ASCII]/L11-ASCII [Basics and Username].py
@@ -179,11 +179,11 @@ def viewdatabase(userdb: dict,admin: bool) -> None:
         for user in userdb:
             password = userdb[f"{user}"]['PASSWORD']
             print(f"{user}:","[")
+            print("    ",style.byellow + "Used Passwords:" + style.RESET,"[")
             print("    ",style.bcyan + "Password:" + style.RESET,f"{'#' * len(password)}")
             print("    ",style.byellow + "Used Passwords:" + style.RESET,"[")
             for usedpassword in userdb[f"{user}"]['USEDPASSWORDS']:
                 print("    ","    ",f"{'#' * len(usedpassword)}")
-            
             print("    ","]")
             print("]")
 
