61a6604cf5e6d1b633178380e79597a291b9c0d8
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-26 10:07:09 +0000

    Apr 26, 2025, 10:07 AM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -6,23 +6,23 @@
 # Pure Python 3.x demo, 256 colors
 # Works with bash under Linux and MacOS
 
-def DebugColour():
-fg = lambda text, color: "\33[38;5;" + str(color) + "m" + text + "\33[0m"
-bg = lambda text, color: "\33[48;5;" + str(color) + "m" + text + "\33[0m"
+def debugcolour():
+    fg = lambda text, color: "\33[38;5;" + str(color) + "m" + text + "\33[0m"
+    bg = lambda text, color: "\33[48;5;" + str(color) + "m" + text + "\33[0m"
 
-def print_six(row, format, end="\n"):
-    for col in range(6):
-        color = row*6 + col - 2
-        if color>=0:
-            text = "{:3d}".format(color)
-            print (format(text,color), end=" ")
-        else:
-            print(end="    ")   # four spaces
-    print(end=end)
+    def print_six(row, format, end="\n"):
+        for col in range(6):
+            color = row*6 + col - 2
+            if color>=0:
+                text = "{:3d}".format(color)
+                print (format(text,color), end=" ")
+            else:
+                print(end="    ")   # four spaces
+        print(end=end)
 
-for row in range(0, 43):
-    print_six(row, fg, " ")
-    print_six(row, bg)
+    for row in range(0, 43):
+        print_six(row, fg, " ")
+        print_six(row, bg)
 
 # Simple usage: print(fg("text", 160))
-
+print(fg)
