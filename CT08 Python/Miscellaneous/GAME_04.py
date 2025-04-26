7c033a04c60603fc7e79f33fe51bfd65ca483b40
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-26 15:30:32 +0000

    Apr 26, 2025, 3:30 PM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -163,4 +163,4 @@ def show_aura_rarity(luck: float = 1.0):
         print(fg(f"Description: {ListedAuras[aura]['description']}", 244))
         print(LINE)
 
-show_aura_rarity(50000)
+show_aura_rarity(1)
