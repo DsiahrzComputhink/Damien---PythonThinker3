d4d15b58d6c5217337c864871b6aa27cf64585eb
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-26 13:55:07 +0000

    Apr 26, 2025, 1:55 PM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -45,15 +45,11 @@ Auras = {
         "description": "very natural thing"
     },
     "Chromatic": {
-        "rarity": 20,
+        "rarity": 200,
         "display": (fg("C", 196) + fg("H", 202) + fg("R", 226) + fg("O", 82) + fg("M", 48) + fg("A", 39) + fg("T", 99) + fg("I", 201) + fg("C", 196) + "                    "),
         "description": "Yes... Feel my unstoppable beats!"
     }
 }
-
-
-
-print(fg("text",160))
 
 def pick_aura():
     weighted_pool = []
