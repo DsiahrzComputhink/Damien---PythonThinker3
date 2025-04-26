77943861183644dc3d1fe92f6fff733584c23e37
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-26 14:54:44 +0000

    Apr 26, 2025, 2:54 PM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -45,6 +45,26 @@ Auras = {
         "display": fg("Natural" + "                    ", 120),
         "description": "very natural thing"
     },
+        "Natural": {
+        "rarity": 8,
+        "display": fg("Natural" + "                    ", 120),
+        "description": "very natural thing"
+    },
+        "Natural": {
+        "rarity": 8,
+        "display": fg("Natural" + "                    ", 120),
+        "description": "very natural thing"
+    },
+        "Natural": {
+        "rarity": 8,
+        "display": fg("Natural" + "                    ", 120),
+        "description": "very natural thing"
+    },
+        "Natural": {
+        "rarity": 8,
+        "display": fg("Natural" + "                    ", 120),
+        "description": "very natural thing"
+    },
     "Chromatic": {
         "rarity": 20000000,
         "display": (fg("C", 196) + fg("H", 202) + fg("R", 226) + fg("O", 82) + fg("M", 48) + fg("A", 39) + fg("T", 99) + fg("I", 201) + fg("C", 196) + "                    "),
