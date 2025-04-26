bb9df006a4fd60dade756e4f6b188fe083384ec8
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-26 13:50:55 +0000

    Apr 26, 2025, 1:50 PM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -26,17 +26,17 @@ debugcolour()
 Auras = {
     "Common": {
         "rarity": 2,
-        "display": fg("Common" + "     ", 255),
+        "display": fg("Common" + "                    ", 255),
         "description": "Very common thing"
     },
     "Uncommon": {
         "rarity": 4,
-        "display": ("Uncommon" + "    "          "      "),
+        "display": ("Uncommon" + "                    "),
         "description": "Yes... Feel my unstoppable beats!"
     },
     "Good": {
         "rarity": 5,
-        "display": fg("Natural" + "     ", 255),
+        "display": fg("Natural" + "                    ", 255),
         "description": "Very common thing"
     },
     "Natural": {
@@ -46,7 +46,7 @@ Auras = {
     },
     "Chromatic": {
         "rarity": 20,
-        "display": (fg("C", 196) + fg("H", 202) + fg("R", 226) + fg("O", 82) + fg("M", 48) + fg("A", 39) + fg("T", 99) + fg("I", 201) + fg("C", 196) + "     "),
+        "display": (fg("C", 196) + fg("H", 202) + fg("R", 226) + fg("O", 82) + fg("M", 48) + fg("A", 39) + fg("T", 99) + fg("I", 201) + fg("C", 196) + "                    "),
         "description": "Yes... Feel my unstoppable beats!"
     }
 }
