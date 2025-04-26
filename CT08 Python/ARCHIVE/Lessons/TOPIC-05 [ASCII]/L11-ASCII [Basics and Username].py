7ca9263d734f94513173d059049d6365a2c01fc5
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-26 00:09:35 +0000

    Apr 26, 2025, 12:09 AM

--- CT08 Python/ARCHIVE/Lessons/TOPIC-05 [ASCII]/L11-ASCII [Basics and Username].py
+++ CT08 Python/ARCHIVE/Lessons/TOPIC-05 [ASCII]/L11-ASCII [Basics and Username].py
@@ -345,7 +345,7 @@ def console():
                                     print(LINE)
                                     print(style.bblue + "Please type in a number")
                                     print(LINE)
-                                    print("{:<13}{:>30}".format(style.bblue + "1" + style.RESET, f"{user}",))
+                                    print("{:<13}{:>30}".format(style.bblue + "1" + style.RESET, "[CLIENT]",))
                                     permNum = input("")
                                 permuser(username,userDatabase,userDatabase[f"{username}"]['PERMS'],'???')
                                 time.sleep(1)
