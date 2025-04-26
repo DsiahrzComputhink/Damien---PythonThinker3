02af4aac956b49ba59db48f66154a74d649e2015
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-26 14:50:45 +0000

    Apr 26, 2025, 2:50 PM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -97,7 +97,7 @@ def roll_animation(luck : int = 1.0):
     print("\n")
     print(LINE)
     print("Rarity:",fg(f"1 / {rarity:,}", 86))
-    print(fg("Actual Chance:",f"1 / {rarity:,}", 86))
+    print(f"Actual Chance:",fg(f"1 / {rarity:,}", 86))
     print(f"{selected_aura['display']}")
     print(fg(f"Description: {selected_aura['description']}", 244))
     print(LINE)
