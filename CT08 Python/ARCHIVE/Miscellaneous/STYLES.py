935b75bfc2272220aa56db9fadbf95ac4f5771a4
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-26 11:52:18 +0000

    Apr 26, 2025, 11:52 AM

--- CT08 Python/ARCHIVE/Miscellaneous/STYLES.py
+++ CT08 Python/ARCHIVE/Miscellaneous/STYLES.py
@@ -77,7 +77,7 @@ def console():
 
 # Default Console Function ⬆
 
-
+# --------------------------------------------------------------------
 fg = lambda text, color: "\33[38;5;" + str(color) + "m" + text + "\33[0m"
 bg = lambda text, color: "\33[48;5;" + str(color) + "m" + text + "\33[0m"
 
