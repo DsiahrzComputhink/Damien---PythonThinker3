0a08a3760ade30b2d8bfff4ea16d78ee281669f1
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-26 11:49:30 +0000

    Apr 26, 2025, 11:49 AM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -30,12 +30,6 @@ debugcolour()
 # Simple usage: print(fg("text", 160))
 print(fg("text",160))
 
-print(fg("C",196) ("H",202) +
-    fg("R",226) +
-    fg("O",82)  +
-    fg("M",48)  +
-    fg("A",39)  +
-    fg("T",99)  +
-    fg("I",201) +
-    fg("C",196)
+print(fg("C",196) ("H",202) + fg("R",226) + fg("O",82) + fg("M",48) + fg("A",39) + fg("T",99) + fg("I",201) +
+fg("C",196)
 )
