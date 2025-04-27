60605e27e328ef4232c66c58b3a24a4d05fcc313
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-27 07:11:09 +0000

    Apr 27, 2025, 7:11 AM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -561,7 +561,7 @@ Auras = {
     "Helios": {
         "rarity": 7000000,
         "display": fg("Helios" + "                    ", 221),
-        "description": "Empty",
+        "description": "In the mythology, A shining god of the sun. They rode their chariot to pull the Sun from the east to the west, making the cycle of day and night",
         "amplify": ['NONE',True]
     },
 
