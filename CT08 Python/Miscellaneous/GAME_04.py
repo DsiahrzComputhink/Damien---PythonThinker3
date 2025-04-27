ca3ea4c0e52af161e78899db24e85811242e76ee
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-27 07:30:10 +0000

    Apr 27, 2025, 7:30 AM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -703,7 +703,7 @@ Auras = {
     },
     "Matrix : Reality": {
         "rarity":  601020102,
-        "display": (fg("MATRIX : REALITY", 175) + "                    "),
+        "display": (fg("MATRIX : REALITY", 145) + "                    "),
         "description": "They were all living in a simulation, This is the TRUE REALITY.",
         "amplify": ['NONE',True]
     },
