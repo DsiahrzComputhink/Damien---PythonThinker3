c205cfeac2318b92fc8592c82e9e82e6f1b90c86
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-27 01:52:27 +0000

    Apr 27, 2025, 1:52 AM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -36,7 +36,8 @@ Biomes = {
     "Starfall": {"Chance": 7500, "Amplify": 5},
     "Corruption": {"Chance": 9000, "Amplify": 5},
     "Null": {"Chance": 10100, "Amplify": 1000},
-    "Glitched": {"Chance": 30000, "Amplify": None}, # allows every biome's native aura to be amplified + some auras exclusive to glitch
+    "Glitched": {"Chance": 30000, "Amplify": 1}, # allows every biome's native aura to be amplified + some auras exclusive to glitch
+    "Dreamspace": {"Chance": 5000000, "Amplify": 1}, # allows every biome's native aura to be amplified + some auras exclusive to glitch
 }
 
 Auras = {
@@ -64,7 +65,7 @@ Auras = {
         "description": "very natural thing",
         "amplify": ['NONE',True]
     },
-
+    # 10
     "Rare": {
         "rarity": 16,
         "display": fg("Rare" + "                    ", 39),
@@ -83,13 +84,19 @@ Auras = {
         "description": "shiny thing",
         "amplify": ['NONE',True]
     },
+    # 100
+    "★": {
+        "rarity": 100,
+        "display": fg("★" + "                    ", 213),
+        "description": "★",
+        "amplify": ['Dreamspace',False]
+    },
     "Rage": {
         "rarity": 128,
         "display": fg("Rage" + "                    ", 160),
         "description": "flame of emotions",
         "amplify": ['NONE',True]
     },
-
     "Topaz": {
         "rarity": 150,
         "display": fg("Topaz" + "                    ", 137),
@@ -114,30 +121,201 @@ Auras = {
         "description": "Gem of a green hue",
         "amplify": ['NONE',True]
     },
+    "Gilded": {
+        "rarity": 500,
+        "display": fg("Gilded" + "                    ", 172),
+        "description": "Thats a shiny one!",
+        "amplify": ['Sandstorm',True]
+    },
+    "Ink": {
+        "rarity": 700,
+        "display": fg("Ink" + "                    ", 240),
+        "description": "Paint all Black",
+        "amplify": ['NONE',True]
+    },
+    "Jackpot": {
+        "rarity": 777,
+        "display": fg("Jackpot" + "                    ", 226),
+        "description": "Thats such a tremendous prize!",
+        "amplify": ['Sandstorm',True]
+    },
+    "Sapphire": {
+        "rarity": 800,
+        "display": fg("Sapphire" + "                    ", 110),
+        "description": "Gem of a azure hue",
+        "amplify": ['NONE',True]
+    },
+    "Aquamarine": {
+        "rarity": 900,
+        "display": fg("Aquamarine" + "                    ", 81),
+        "description": "Empty",
+        "amplify": ['NONE',True]
+    },
+     "Wind": {
+        "rarity": 900,
+        "display": fg("Wind" + "                    ", 87),
+        "description": "The wind is hovering around you",
+        "amplify": ['Windy',True]
+    },
 
+    # 1,000
+    "★★": {
+        "rarity": 1000,
+        "display": fg("★ ★" + "                    ", 213),
+        "description": "★ ★",
+        "amplify": ['Dreamspace',False]
+    },
+    "Diaboli": {
+        "rarity": 1004,
+        "display": fg("Diaboli" + "                    ", 126),
+        "description": "Empty",
+        "amplify": ['NONE',True]
+    },
+    "Precious": {
+        "rarity": 1024,
+        "display": fg("Precious" + "                    ", 45),
+        "description": "Empty",
+        "amplify": ['NONE',True]
+    },
+    "Glock": {
+        "rarity": 1700,
+        "display": fg("Glock" + "                    ", 240),
+        "description": "What do you mean by aura??? its just a Glock-17!",
+        "amplify": ['NONE',True]
+    },
+    "Magnetic": {
+        "rarity": 2048,
+        "display": fg("Magnetic" + "                    ", 91),
+        "description": "Its surrounded by powerful magnetic force",
+        "amplify": ['NONE',True]
+    },
+    "Ash": {
+        "rarity": 2300,
+        "display": fg("Ash" + "                    ", 7),
+        "description": "Everything that is destroyed goes back to ashes",
+        "amplify": ['NONE',True]
+    },
+    "Glacier": {
+        "rarity": 2304,
+        "display": fg("Glacier" + "                    ", 81),
+        "description": "a cold spirit",
+        "amplify": ['Snowy',True]
+    },
+
+    "Fault": {
+        "rarity": 3000,
+        "display": (fg("F", 34) + fg("a", 22) + fg("u", 34) + fg("l", 28) + fg("t", 22) + "                    "),
+        "description": "a heterogeneous substance",
+        "amplify": ['Glitched',False]
+    },
+    "Player": {
+        "rarity": 3000,
+        "display": ("Player" + "                    "),
+        "description": "a heterogeneous substance",
+        "amplify": ['Glitched',False]
+    },
+    "Siderium": {
+        "rarity": 4096,
+        "display": (fg("Side", 220) + fg("reum", 170) + "                    "),
+        "description": "A trail of broken stars",
+        "amplify": ['Glitched',False]
+    },
+    "Bleeding": {
+    "rarity": 4444,
+        "display": (fg("Bleeding", 52) + "                    "),
+        "description": "Endlessly oozing blood... Hey, you sure alright?",
+        "amplify": ['Glitched',False]
+    },
+    "Flushed": {
+        "rarity": 6900,
+        "display": (fg("Flushed", 220) + "                    "),
+        "description": "flushed",
+        "amplify": ['Glitched',False]
+    },
+
+    # 10,000
+    "★★★": {
+        "rarity": 10000,
+        "display": fg("★ ★ ★" + "                    ", 213),
+        "description": "★ ★ ★",
+        "amplify": ['Dreamspace',False]
+    },
     "Rage:Heated": {
         "rarity": 12800,
         "display": fg("Rage:Heated" + "                    ", 202),
         "description": "furious emotions",
         "amplify": ['NONE',True]
     },
+    "Ink:LEAK": {
+        "rarity": 14000,
+        "display": fg("Ink : Leak" + "                    ", 240),
+        "description": "Paint all Black",
+        "amplify": ['NONE',True]
+    },
 
+    # 100,000
+    "Diaboli:Void": {
+        "rarity": 100400,
+        "display": fg("Diaboli : Void" + "                    ", 55),
+        "description": "Empty",
+        "amplify": ['NONE',True]
+    },
+
+    # 1,000,000
+    "Magnetic:Reverse Polarity": {
+        "rarity": 1024000,
+        "display": fg("Magnetic : Reverse Polarity" + "                    ", 127),
+        "description": "A collection of strong energies with inverted directions of acting polarities, which are not described magnetically.",
+        "amplify": ['NONE',True]
+    },
     "Rage:Brawler": {
         "rarity": 1280000,
-        "display": fg("Rage:Brawler" + "                    ", 196),
+        "display": fg("Rage : Brawler" + "                    ", 196),
         "description": "flame of emotions",
         "amplify": ['NONE',True]
+    },
+
+    # 10,000,000
+    "Glitched": {
+        "rarity": 12210110,
+        "display": (fg("G", 22) + fg("L", 28) + fg("1", 22) + fg("T", 28) + fg("C", 34) + fg("H", 28) + "                    "),
+        "description": ".-- .... .- - ..--.. -. --- --..-- .. - .----. ... .-. .. -.. .. -.-. ..- .-.. --- ..- ... .-.-.- .. - ... .... --- ..- .-.. -.. -. .----. - .... .- .--. .--. . -.",
+        "amplify": ['Glitched',False]
     },
 
     "Chromatic": {
         "rarity": 20000000,
         "display": (fg("C", 196) + fg("H", 202) + fg("R", 226) + fg("O", 82) + fg("M", 48) + fg("A", 39) + fg("T", 99) + fg("I", 201) + fg("C", 196) + "                    "),
         "description": "Yes... Feel my unstoppable beats!"
-    }
+    },
+
+    # 99,999,999 - Globals
+    "Chromatic:Genesis": {
+        "rarity": 99999999,
+        "display": (fg("CHROMATIC : GENESIS", 153) + "                    "),
+        "description": "WAKE UP FROM AWAY"
+    },
+
+    "Oppression": {
+        "rarity": 220000000,
+        "display": (fg("O", 255) + fg("p", 249) + fg("p", 238) + fg("r", 255) + fg("e", 238) + fg("s", 245) + fg("s", 250) + fg("i", 238) + fg("o", 243) + fg("n", 247) + "                    "),
+        "description": "... is this truly the end?"
+    },
+
+    "Pixelation": {
+        "rarity": 220000000,
+        "display": (fg("O", 255) + fg("p", 249) + fg("p", 238) + fg("r", 255) + fg("e", 238) + fg("s", 245) + fg("s", 250) + fg("i", 238) + fg("o", 243) + fg("n", 247) + "                    "),
+        "description": "... is this truly the end?"
+    },
+    "Luminosity": {
+        "rarity": 200000000,
+        "display": (fg("O", 255) + fg("p", 249) + fg("p", 238) + fg("r", 255) + fg("e", 238) + fg("s", 245) + fg("s", 250) + fg("i", 238) + fg("o", 243) + fg("n", 247) + "                    "),
+        "description": "... is this truly the end?"
+    },
 }
 
 
-def format_chance(num):
+def format_chance(num) -> None:
     if num < 10:
         return f"{num:.3f}"
     elif num < 100:
@@ -147,20 +325,50 @@ def format_chance(num):
     else:
         return f"{num:,.0f}"
 
-def show_aura_rarity(luck: float = 1.0):
+def show_aura_rarity(luck: float = 1.0, currentbiome: str = "None"):
     ListedAuras = {}
-    for aura in Auras:
-        if luck < Auras[aura]["rarity"]:
-            ListedAuras[aura] = Auras[aura]
-    for aura in ListedAuras:
-        rarity = ListedAuras[aura]["rarity"]
+
+    for aura_name, aura_info in Auras.items():
+        aura_copy = aura_info.copy()
+
+        amplify_info = aura_copy.get('amplify', ['NONE', True])
+        native_biome = amplify_info[0]
+        biome_lock = amplify_info[1] if len(amplify_info) > 1 else True  # Default True if missing
+
+        # check if biome-locked
+        if not biome_lock:
+            if currentbiome != native_biome:
+                continue
+
+        # if glitched biome
+        if currentbiome == 'Glitched':
+            if native_biome != 'NONE':
+                biome_data = Biomes.get(native_biome)
+                if biome_data and biome_data.get('Amplify'):
+                    aura_copy['rarity'] /= biome_data['Amplify']
+
+        # usual biome
+        elif currentbiome != 'None':
+            biome_data = Biomes.get(currentbiome)
+            if biome_data and native_biome == currentbiome:
+                if biome_data.get('Amplify'):
+                    aura_copy['rarity'] /= biome_data['Amplify']
+
+        ListedAuras[aura_name] = aura_copy
+
+    # display
+    for aura_name, aura_info in ListedAuras.items():
+        rarity = aura_info["rarity"]
         actual_chance = rarity / luck
 
+        if luck >= rarity:
+            continue
         print(LINE)
-        print("Rarity:", fg(f"1 / {rarity:,}", 81))
-        print(f"Actual Chance:", fg(f"1 / {format_chance(actual_chance)}", 75))
-        print(f"{ListedAuras[aura]['display']}")
-        print(fg(f"Description: {ListedAuras[aura]['description']}", 244))
+        print("Rarity:", fg(f"1 / {int(rarity):,}", 81))
+        print("Actual Chance:", fg(f"1 / {format_chance(actual_chance)}", 75))
+        print(aura_info['display'])
+        print(fg(f"Description: {aura_info['description']}", 244))
         print(LINE)
 
-show_aura_rarity(1)
+
+show_aura_rarity(15.39,'Dreamspace')
