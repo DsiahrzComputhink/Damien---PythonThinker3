a1ab7623190387cce7458f2b7b36e0a0a64051c0
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-27 08:29:49 +0000

    Apr 27, 2025, 8:29 AM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -910,8 +910,8 @@ Tiers = [
     {"name": "UNIQUE", "range": (9999, 99998), "color": 179},
     {"name": "LEGENDARY", "range": (99999, 999998), "color": 220},
     {"name": "MYTHIC", "range": (999999, 9999998), "color": 213},
-    {"name": "EXALTED", "range": (9999999, 99,99,998), "color": 27},
-    {"name": "GLORIOUS", "range": (99999999, 999999998), "color": 160},
+    {"name": "EXALTED", "range": (9,999,999, 99,999,998), "color": 27},
+    {"name": "GLORIOUS", "range": (99,999,999, 999999998), "color": 160},
     {"name": "TRANSCENDENT", "range": (999999999, float('inf')), "color": 199},
 ]
 
