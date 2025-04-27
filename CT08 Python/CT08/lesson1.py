29e671f93f3d3cf5a1a9a5cce6261b37d9b6a5ae
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-27 04:13:54 +0000

    Apr 27, 2025, 4:13 AM

--- CT08 Python/CT08/lesson1.py
+++ CT08 Python/CT08/lesson1.py
@@ -15,6 +15,7 @@ book_id = input("Enter the book ID: ")
 if action == "b":
     if books[book_id] == "AVAILABLE":
         books[book_id] == "BORROWED"
+        print(bo)
         print("You have borrowed the book.")
     else:
         print("The book is already borrowed.")
