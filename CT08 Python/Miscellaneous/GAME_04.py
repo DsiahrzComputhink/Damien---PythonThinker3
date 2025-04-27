5feb4d0986b54ecb29f4f697c6e43b409c95039f
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-27 07:57:38 +0000

    Apr 27, 2025, 7:57 AM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -1,3 +1,42 @@
+class style():
+
+    BOLD = '\033[1m'
+    ITALIC = '\033[3m'
+
+    UNDERLINE = '\033[4m'
+    CANCEL = '\033[9m'
+
+    bgbwhite = '\033[7m'
+
+    black = '\033[8m'
+    bgray = '\033[30m'
+    dred = '\033[31m'
+    dgreen = '\033[32m'
+    dyellow = '\033[33m'
+    dblue = '\033[34m'
+    dpurple = '\033[35m'
+    dcyan = '\033[36m'
+    dwhite = '\033[37m'
+
+    bgray = '\033[90m'
+    bred = '\033[91m'
+    bgreen = '\033[92m'
+    byellow = '\033[93m'
+    bblue = '\033[94m'
+    bpurple = '\033[95m'
+    bcyan = '\033[96m'
+    bwhite = '\033[97m'
+
+    RESET = '\033[0m'
+
+    # Generic Colours
+    primary = dblue
+    secondary = bgray
+    
+    warning = dyellow
+    error = dred
+LINE = style.bgray + "------------------------------" + style.RESET
+
 import time
 import random
 import sys
@@ -887,6 +926,6 @@ def console():
                 stop += 1   
         else:
             print(style.bred + "Command does not exist" + style.RESET)
-            
+
 show_aura_rarity(1 * 10000 * 1000,'Normal')
 debugcolour()
