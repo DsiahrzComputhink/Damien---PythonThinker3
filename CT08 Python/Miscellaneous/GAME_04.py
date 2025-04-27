1834ddb323a60b7098aabccd3dffa3c9d4dbba1d
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-27 08:32:10 +0000

    Apr 27, 2025, 8:32 AM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -906,7 +906,7 @@ def show_aura_rarity(luck: float = 1.0, currentbiome: str = "None"):
 # Define Tiers
 Tiers = [
     {"name": "BASIC", "range": (1, 999), "color": 250},
-    {"name": "EPIC", "range": (1000, 9998), "color": 182},
+    {"name": "EPIC", "range": (1_000, 9_998), "color": 182},
     {"name": "UNIQUE", "range": (9999, 99998), "color": 179},
     {"name": "LEGENDARY", "range": (99999, 999998), "color": 220},
     {"name": "MYTHIC", "range": (999999, 9999998), "color": 213},
