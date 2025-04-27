b2c8f366713ec3762d15c2c8e8157eb640d84318
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-27 03:24:08 +0000

    Apr 27, 2025, 3:24 AM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -257,6 +257,12 @@ Auras = {
         "description": "★ ★ ★",
         "amplify": ['Dreamspace',False]
     },
+    "Undead": {
+        "rarity": 12000,
+        "display": (fg("Undead", 22) + "                    "),
+        "description": "failed to die...",
+        "amplify": ['NONE',True]
+    },
     "Rage:Heated": {
         "rarity": 12800,
         "display": fg("Rage:Heated" + "                    ", 202),
