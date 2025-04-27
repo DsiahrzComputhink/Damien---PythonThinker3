b35f0e3fd85db3a4ffe0f7a12e5c877d89d7952a
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-27 06:16:53 +0000

    Apr 27, 2025, 6:16 AM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -402,9 +402,9 @@ Auras = {
     },
 
     # 1,000,000 - Mythic
-    "Magnetic : Reverse Polarity": {
+    "Bounded": {
         "rarity": 1024000,
-        "display": fg("Magnetic : Reverse Polarity" + "                    ", 127),
+        "display": fg("BOUNDED" + "                    ", 127),
         "description": "A collection of strong energies with inverted directions of acting polarities, which are not described magnetically.",
         "amplify": ['NONE',True]
     },
