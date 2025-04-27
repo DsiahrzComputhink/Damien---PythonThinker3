227d8c355ae266e02ee1a27caa0da9013fc3a42a
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-27 08:06:14 +0000

    Apr 27, 2025, 8:06 AM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -957,7 +957,6 @@ def roll_for_aura(luck: float = 1.0, currentbiome: str = "None"):
     aura_info = ListedAuras[result]
     print(LINE)
     print(fg("YOU ROLLED:", 220), aura_info['display'])
-    print()
     print(fg(f"'{aura_info['description']}'", 244))
     print(LINE)
     return result
