bf1fd6bf0fe48649b5a1fe527fd7cc0414fa640f
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-27 04:04:33 +0000

    Apr 27, 2025, 4:04 AM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -428,7 +428,7 @@ Auras = {
     },
     "Exotic : VOID": {
         "rarity": 299999999,
-        "display": (fg("Exotic ◇ VOID", 85) + "                    "),
+        "display": (fg("Exotic ◇ VOID", 92) + "                    "),
         "description": "Nobody knows where it originates from, and how old it is.",
         "amplify": ['NONE',True]
     },
