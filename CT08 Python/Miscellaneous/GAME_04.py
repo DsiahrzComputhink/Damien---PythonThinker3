38acecfbf938f28585e5eac81507f009f133e599
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-27 07:01:14 +0000

    Apr 27, 2025, 7:01 AM

--- CT08 Python/Miscellaneous/GAME_04.py
+++ CT08 Python/Miscellaneous/GAME_04.py
@@ -525,7 +525,7 @@ Auras = {
     "Hades": {
         "rarity": 6666666,
         "display": fg("Hades" + "                    ", 166),
-        "description": "A jewel that shines brightly once every thousand years in the deepest part of the forest.",
+        "description": "It has been shaped as the ruler of hell, a long time ago. Though not replicated perfectly, it still remains cruel and callous.",
         "amplify": ['Hell',True]
     },
 
