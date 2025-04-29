919be40a8a33a5b5b107de96ee98254a8da910a7
DsiahrzComputhink <damiensiah11@gmail.com>
2025-04-29 08:58:19 +0000

    Apr 29, 2025, 8:58 AM

--- GAME-05.py
+++ GAME-05.py
@@ -905,7 +905,7 @@ def roll_animation(luck : int = 1.0):
     selected_aura = pick_aura(luck)
     sys.stdout.write("\r" + fg(f"You rolled {selected_aura['display']}!              ", random.randint(232, 255)))
     sys.stdout.flush()
-    print("HI")
+    print("\n")
 
     rarity = selected_aura['rarity']
 
