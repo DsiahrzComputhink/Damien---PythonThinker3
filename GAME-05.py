d88057140e3ac5b5e50aec6ac083f46da31b6bc8
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-01 08:00:16 +0000

    May 1, 2025, 8:00 AM

--- GAME-05.py
+++ GAME-05.py
@@ -910,7 +910,7 @@ def Roll(luck: float = 1.0, rollspeed: float = 1.0):
 
     sys.stdout.write("\r" + fg(f"You rolled {display_name} !              ", random.randint(232, 255)))
     sys.stdout.flush()
-    print(\n)
+    print(f"\n")
 
     # Update inventory using display
     if display_name in InventoryAuras:
