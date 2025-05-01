75b0f454881ee0b02f662c55ced3e578af26b2d7
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-01 07:51:59 +0000

    May 1, 2025, 7:51 AM

--- GAME-05.py
+++ GAME-05.py
@@ -951,7 +951,7 @@ def craft(recipe: dict):
             InventoryAuras[display] = 1
     else:
         print(f"{fg("Not enough Resources.",160)}")
-        print(inventory(InventoryAuras, {fg("Not enough Resources", 160)}))
+        print(inventory(InventoryAuras, f"{fg("Not enough Resources", 160)}"))
 
 
 
