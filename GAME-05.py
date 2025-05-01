be5544eda88880183a1a6b7166a778f24646276c
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-01 09:16:48 +0000

    May 1, 2025, 9:16 AM

--- GAME-05.py
+++ GAME-05.py
@@ -965,7 +965,7 @@ Recipes = {}
 Recipes["Gear Basing"] = {Auras["Rare"]["display"]: 1,Auras["Good"]["display"]: 1, Auras["Uncommon"]["display"]: 1, Auras["Common"]["display"]: 1,"Display": fg("Gear Basing", 245)}
 Recipes["Luck Glove"] = {Recipes["Gear Basing"]["Display"]: 1,Auras["Rare"]["display"]: 3,Auras["Divinus"]["display"]: 2, Auras["Crystallized"]["display"]: 1, "Display": fg("Luck Glove", 83)}
 
-Recipes["Luck Glove"] = {
+Recipes["Lunar Device"] = {
     Recipes["Gear Basing"]["Display"]: 1,
     Auras["Rare"]["display"]: 3,
     Auras["Divinus"]["display"]: 2, 
