fdae04da1720b5f4cbc17bc1652377683a181dfe
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-01 08:32:19 +0000

    May 1, 2025, 8:32 AM

--- GAME-05.py
+++ GAME-05.py
@@ -961,7 +961,14 @@ def inventory(inventory : dict,name: str) -> None:
     print(LINE)
 
 Recipes = {
-    "Gear Basing": {Auras["Common"]["display"] : 1, Auras["Uncommon"]["display"] : 1, Auras["Good"]["display"] : 1, Auras["Rare"]["display"] : 1, "Display" : fg("Gear Basing",245)}
+    "Gear Basing": {Auras["Common"]["display"] : 1, Auras["Uncommon"]["display"] : 1, Auras["Good"]["display"] : 1, Auras["Rare"]["display"] : 1, "Display" : fg("Gear Basing",245)},
+    "Darkshader": {
+        Auras["Common"]["display"] : 1,
+        Auras["Uncommon"]["display"] : 1,
+        Auras["Good"]["display"] : 1, 
+        Auras["Rare"]["display"] : 1, 
+        "Display" : fg("Gear Basing",245)
+        },
 }
 
 # LUCK = ((1 + Basic Luck) * Bonus Roll + Special Buff) * VIP
