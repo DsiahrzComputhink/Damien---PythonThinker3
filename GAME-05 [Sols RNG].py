100ddd80eec0d7f5c088290e63ad7074e5e3a63d
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-01 15:49:56 +0000

    May 1, 2025, 3:49 PM

--- GAME-05 [Sols RNG].py
+++ GAME-05 [Sols RNG].py
@@ -1047,10 +1047,10 @@ Recipes["Eclipse Device"] = {
 }
 
 Recipes["Exo Gauntlet"] = {
-    Recipes["Gear Basing"]["display"]: 3,
-    Auras["Exotic"]["display"]: 1,
+    Recipes["Gear Basing"]["display"]: 3,,
     Auras["Divinus"]["display"]: 1, 
-    Auras["Solar"]["display"]: 1, 
+    Auras["Undead"]["display"]: 1, 
+    Auras["Exotic"]["display"]: 1,
     "display": f"{fg("[T3]",81)} {fg("Exo Gauntlet", 117)}",
     "luck": 2.00,
     "rollspeed": 1.20,
