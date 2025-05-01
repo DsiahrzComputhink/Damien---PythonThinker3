abbab320e9cd7c9a9211fece7923bf8a70766de4
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-01 16:14:13 +0000

    May 1, 2025, 4:14 PM

--- GAME-05 [Sols RNG].py
+++ GAME-05 [Sols RNG].py
@@ -1063,7 +1063,7 @@ Recipes["Exo Gauntlet"] = {
 Recipes["Subzero Device"] = {
     Recipes["Gear Basing"]["display"]: 5,
     Auras["Solar"]["display"]: 15,
-    Auras[""]["display"]: 15,
+    Auras["Crystallized"]["display"]: 15,
     Auras["Precious"]["display"]: 40,
     Auras["Magnetic"]["display"]: 20, 
     Auras["Sidereum"]["display"]: 10, 
