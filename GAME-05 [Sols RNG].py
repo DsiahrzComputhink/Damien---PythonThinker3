ddf89dd7ac3a144204db7809a3710422b06fbbbf
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-02 04:14:37 +0000

    May 2, 2025, 4:14 AM

--- GAME-05 [Sols RNG].py
+++ GAME-05 [Sols RNG].py
@@ -998,7 +998,7 @@ Recipes["Eclipse"] = {
 Recipes["Matrix : Steampunk"] = {
     Auras["Solar"]["display"]: 1, 
     Auras["Lunar"]["display"]: 1, 
-    "display": f"{fg('MATRIX : STEAMPUNK', 215)} {fg('[UNIQUE CRAFTED]',179)}",
+    "display": f"{fg('MATRIX : STEAMPUNK', 215)} {fg('[GLORIOUS CRAFTED]',179)}",
     "type": "Aura"
 }
 
