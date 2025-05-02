449cd8962e96ef88114592b5f4c9c2c62eaab0e4
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-02 03:38:14 +0000

    May 2, 2025, 3:38 AM

--- GAME-05 [Sols RNG].py
+++ GAME-05 [Sols RNG].py
@@ -1206,7 +1206,7 @@ Recipes["Neuralyzer"] = {
     Auras["Poseidon"]["display"]: 3,
     Auras["Zeus"]["display"]: 3,
     Auras["Hades"]["display"]: 3,
-    "display": f"{fg("[T9]",177)} {fg("Ne", 216)}{fg("ur", 222)}{fg("al", 157)}{fg("yz", 159)}{fg("er", 159)}",
+    "display": f"{fg("[T9]",177)} {fg("Ne", 216)}{fg("ur", 222)}{fg("al", 157)}{fg("yz", 159)}{fg("er", 183)}",
     "luck": 1.00 + 4.55,
     "rollspeed": 1.00 + 0.40,
     "type": "Gauntlet"
