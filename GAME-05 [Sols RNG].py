82d946d927283c16f39e368990fa64937d2978a8
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-02 04:08:36 +0000

    May 2, 2025, 4:08 AM

--- GAME-05 [Sols RNG].py
+++ GAME-05 [Sols RNG].py
@@ -1224,7 +1224,7 @@ Recipes["Starshaper"] = {
     Auras["Galaxy"]["display"]: 3,
     Auras["Hypervolt"]["display"]: 3,
     Auras["Starscourge"]["display"]: 2,
-    "display": f"{fg("[T8]",141)} {fg("Starshaper", 216)}",
+    "display": f"{fg("[T8]",141)} {fg("Starshaper", 219)}",
     "luck": 1.00 + 4.55,
     "rollspeed": 1.00 + 0.40,
     "type": "Gauntlet"
