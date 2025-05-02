d45428eebf228f8384bf98973690317bb1452323
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-02 04:52:21 +0000

    May 2, 2025, 4:52 AM

--- GAME-05 [Sols RNG].py
+++ GAME-05 [Sols RNG].py
@@ -811,7 +811,8 @@ def console():
                 print(LINE)
                 print(style.bblue + "1" + style.RESET,"                ","Change Password")
                 print(style.bblue + "2" + style.RESET,"                  ","View Commands")
-                print(style.bblue + "3" + style.RESET,"                       ","Sign Out")
+                print(style.bblue + "3" + style.RESET,"                  ","View Commands")
+                print(style.bblue + "4" + style.RESET,"                       ","Sign Out")
                 print(LINE)
                 command = input("")
                 if command.isnumeric():
