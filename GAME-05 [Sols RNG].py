6f14199d24c6a17ddaf01589128a1abd9e473c49
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-02 02:56:37 +0000

    May 2, 2025, 2:56 AM

--- GAME-05 [Sols RNG].py
+++ GAME-05 [Sols RNG].py
@@ -908,6 +908,8 @@ def Roll(luck: float = 1.0, rollspeed: float = 1.0):
 
     sys.stdout.write("\r" + fg(f"You rolled {display_name} !                                              ", random.randint(232, 255)))
     sys.stdout.flush()
+    sys.stdout.write("\r" + fg(f"You rolled {display_name} !", random.randint(232, 255)))
+    sys.stdout.flush()
     print(f"\n")
 
     # Update inventory using display
