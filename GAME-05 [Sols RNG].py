6a388c2820b807ef07c368b62552ce35f00f2e6b
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-02 04:24:01 +0000

    May 2, 2025, 4:24 AM

--- GAME-05 [Sols RNG].py
+++ GAME-05 [Sols RNG].py
@@ -956,23 +956,19 @@ def craft(recipe: dict):
     else:
         inventory(Incomplete, f"{fg(f"Not enough Resources",160)} for {recipe['display']}")
 
-def inventory(inventory: dict, name: str) -> None:
+def inventory(inventory : dict,name: str) -> None:
     print(LINE)
-    print(name)
+    print(f"{name}")
     print(LINE)
     remove = []
-    for item_name, count in inventory.items():
-        if isinstance(count, (int, float)):
-            if count == 0:
-                remove.append(item_name)
-            else:
-                print(f"{item_name} : {count:,}")
+    for name, count in inventory.items():
+        if count == 0:
+            remove.append(name)
         else:
-            print(f"{item_name} : {count}")  # Safe for strings like "0 / 1"
+            print(f"{name} : {count}")
     print(LINE)
     for item in remove:
         del inventory[item]
-
 
 def equip(recipe: str):
     global luck, rollspeed
