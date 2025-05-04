d663a667de948bb13a27ae4477dfc6ddddd4d042
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-04 03:40:05 +0000

    May 4, 2025, 3:40 AM

--- CT08 Python/ARCHIVE/Lessons/[EXAMS]/[EXAM] [2] End Semester Practical.py
+++ CT08 Python/ARCHIVE/Lessons/[EXAMS]/[EXAM] [2] End Semester Practical.py
@@ -110,7 +110,6 @@ vip_dictionary = {}
 # Part 1
 for name in customer_spending:
     if customer_spending[name] > 1000:
-        print(f"Hi {name}, you are now a VIP! Congratulations!")
         vip_dictionary[name] = customer_spending[name] # Part 1
     else:
         money_spent = customer_spending[name]
