fe3083ba8355234ed3cb7f72e00230966ecaebe0
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-04 03:42:20 +0000

    May 4, 2025, 3:42 AM

--- CT08 Python/ARCHIVE/Lessons/[EXAMS]/[EXAM] [2] End Semester Practical.py
+++ CT08 Python/ARCHIVE/Lessons/[EXAMS]/[EXAM] [2] End Semester Practical.py
@@ -113,7 +113,7 @@ for name in customer_spending:
     if customer_spending[name] > 1000:
         vip_dictionary[name] = customer_spending[name] # if over $1000 spent, copy customer_spending[name] into vip_dictionary
     else:
-        non_vip_dictionary = customer_spending[name] # if under $1000 spent, copy customer_spending[name] into non_vip_dictionary
+        non_vip_dictionary[name] = customer_spending[name] # if under $1000 spent, copy customer_spending[name] into non_vip_dictionary
 
 print(vip_dictionary)
 
@@ -138,6 +138,7 @@ for name in vip_dictionary:
 
 # Write your code here, add more space as required
 
+print(LINE)
 
 for name in non_vip_dictionary:
     difference = 1000 - non_vip_dictionary[name]
