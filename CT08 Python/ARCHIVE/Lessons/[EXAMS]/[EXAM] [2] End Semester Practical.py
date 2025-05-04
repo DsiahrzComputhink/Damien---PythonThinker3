711ff0e2c93df13cffe72a63f3d402ba6c89f0ec
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-04 03:29:19 +0000

    May 4, 2025, 3:29 AM

--- CT08 Python/ARCHIVE/Lessons/[EXAMS]/[EXAM] [2] End Semester Practical.py
+++ CT08 Python/ARCHIVE/Lessons/[EXAMS]/[EXAM] [2] End Semester Practical.py
@@ -47,6 +47,6 @@ print("Welcome to the Restruant!")
 print(LINE)
 print("MENU:")
 for key in menu:
-    print(f"{key} : ${menu[key]:8.2f}")
+    print(f"{key} : ${menu[key]:0.2f}")
 
 ##########################################################
