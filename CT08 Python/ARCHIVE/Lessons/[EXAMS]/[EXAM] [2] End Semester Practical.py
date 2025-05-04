61633af87b97fa8342abb9c0e0a5bda068ea407c
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-04 03:31:00 +0000

    May 4, 2025, 3:31 AM

--- CT08 Python/ARCHIVE/Lessons/[EXAMS]/[EXAM] [2] End Semester Practical.py
+++ CT08 Python/ARCHIVE/Lessons/[EXAMS]/[EXAM] [2] End Semester Practical.py
@@ -53,6 +53,6 @@ while True:
     print("What would you like to order?")
     order = input("")
     if order.capitalize() in menu:
-        print(f"That item costs {menu}")
+        print(f"That item costs ${menu[order]}")
 
 ##########################################################
