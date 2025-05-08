e221af22407b557b7775bf1a4677156b3595d2ed
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-08 05:09:33 +0000

    May 8, 2025, 5:09 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -1,17 +1,1 @@
 # hi
-import random
-list = [
-    "iron",
-    "captain",
-    "thor",
-    "girl",
-    "loki",
-]
-
-def random():
-    import random
-    num = random.randint(0,4)
-    print(list[num])
-
-for i in range(3):
-    random()
