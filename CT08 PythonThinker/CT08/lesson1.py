dd26549e05f2d23fa1bf8e06006bdbc8001fba43
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-11 03:20:02 +0000

    May 11, 2025, 3:20 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -31,7 +31,7 @@ for i in range(len(egg_code)):
         check += 1
     if egg_code[i][0] in [valid]:
         check += 1
-        EggMethod = fg(egg_code[i][0],69)
+        EggMethod = f"{fg(egg_code[i][0],69)}"
     if egg_code[i][1:3].isalpha:
         check += 1
         EggCountry = f"{fg(egg_code[i][1:3],82)}"
