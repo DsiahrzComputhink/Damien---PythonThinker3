81c5f0365ff9bf7e290c5ea5ebcb56533981db44
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-11 04:24:51 +0000

    May 11, 2025, 4:24 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -41,15 +41,14 @@ name1.printattributes()
         # Makes the code intitutive and easy to understand
         # Each Object has their own memory
 
-# Key Concepts in Classes:
-# Class: A blueprint or templatefor creating objects
-# Defines shared traits and behaviours
-
-# Object:
-    # A specific instnce of a class
-# Attributes: 
-    # Data that described a object
-# Methods:
-    # Actions or behaviours that an object can perform
-# Parent Class and Child Objects:
-    # A class acts as a parent, and objects are its children
+# -- Key Concepts in Classes:
+    # Class: A blueprint or templatefor creating objects
+        # Defines shared traits and behaviours
+    # Object:
+        # A specific instnce of a class
+    # Attributes: 
+        # Data that described a object
+    # Methods:
+        # Actions or behaviours that an object can perform
+    # Parent Class and Child Objects:
+        # A class acts as a parent, and objects are its children
