636ff42797cb03b4313eb6e4d73ad2e83bc4644c
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-11 03:12:34 +0000

    May 11, 2025, 3:12 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -1,3 +1,26 @@
+# --------------------------------------------------------------------
+fg = lambda text, color: "\33[38;5;" + str(color) + "m" + text + "\33[0m"
+bg = lambda text, color: "\33[48;5;" + str(color) + "m" + text + "\33[0m"
+LINE = fg("------------------------------",235)
+
+def debugcolour():
+
+    def print_six(row, format, end="\n"):
+        for col in range(6):
+            color = row*6 + col - 2
+            if color>=0:
+                text = "{:3d}".format(color)
+                print (format(text,color), end=" ")
+            else:
+                print(end="    ")   # four spaces
+        print(end=end)
+
+    for row in range(0, 43):
+        print_six(row, fg, " ")
+        print_six(row, bg)
+
+debugcolour()
+
 egg_code = ['1UK42211','2FR9292','1UK29292','0NL24555','0NL93933']
 valid = 0
 
@@ -13,11 +36,8 @@ for i in range(len(egg_code)):
         check += 1
     if check == 3:
         valid += 1
-    print(egg_code[i][1:3])
-    print(egg_code[i][3:])
-
-print(len(egg_code))
-print(valid)
+        # for visuals
+        print(f"{"hi"}")
 if valid == len(egg_code):
     print("Codes for the entire batch of eggs are valid.")
     # Collate the number of eggs sampled according to farm method
