74c78cd986418a40ec5a1eea3c4b6f31d51318b5
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-11 03:04:31 +0000

    May 11, 2025, 3:04 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -1,1 +1,25 @@
-# hi
+# --------------------------------------------------------------------
+fg = lambda text, color: "\33[38;5;" + str(color) + "m" + text + "\33[0m"
+bg = lambda text, color: "\33[48;5;" + str(color) + "m" + text + "\33[0m"
+LINE = fg("------------------------------",235)
+
+def debugcolour():
+
+    def print_six(row, format, end="\n"):
+        for col in range(6):
+            color = row*6 + col - 2
+            if color>=0:
+                text = "{:3d}".format(color)
+                print (format(text,color), end=" ")
+            else:
+                print(end="    ")   # four spaces
+        print(end=end)
+
+    for row in range(0, 43):
+        print_six(row, fg, " ")
+        print_six(row, bg)
+
+debugcolour()
+
+# Simple usage: print(fg("text", 160))
+# Advanced Text Colour ⬆
