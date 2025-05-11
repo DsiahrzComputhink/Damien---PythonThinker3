5edf62aeaa81a3ab65f79bd9bc7a62ecd7e0ae37
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-11 04:09:27 +0000

    May 11, 2025, 4:09 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -12,9 +12,11 @@ Placeholder = {
     }
 }
 
+# We can do:
 class Placeholder:
     def __init__(self,behaviour1,behaviour2,behaviour3):
         self.behaviour1 = behaviour1
         self.behaviour2 = behaviour2
         self.behaviour3 = behaviour3
+        
 # 'self' represents the instance of the class on which the method is called. 
