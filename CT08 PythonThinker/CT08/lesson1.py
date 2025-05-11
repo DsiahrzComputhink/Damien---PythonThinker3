a5427155f590c6bdda16fada8ab493bd1be8e9db
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-11 04:06:20 +0000

    May 11, 2025, 4:06 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -2,3 +2,7 @@
     # OOP is a way of organizing code that is based on real-world objects.
     # A Class will have Attribute and Behaviour
     # Class: A blueprint that defines what an object should have and what it can do.
+
+class Placeholder:
+    def __init__(behaviour1,behaviour2,behaviour3):
+        pass
