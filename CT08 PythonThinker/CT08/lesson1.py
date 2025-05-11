bf98d586b3c8741a0aad23f4a61803b9f3ddcf05
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-11 04:09:01 +0000

    May 11, 2025, 4:09 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -14,3 +14,4 @@ class Placeholder:
         self.behaviour1 = behaviour1
         self.behaviour2 = behaviour2
         self.behaviour3 = behaviour3
+# 'self' represents the instance of the class on which the method is called. 
