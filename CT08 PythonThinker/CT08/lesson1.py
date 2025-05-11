3d7235eeff71e8fe872ce67a5f13f25322cd2d64
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-11 04:04:35 +0000

    May 11, 2025, 4:04 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -1,5 +1,4 @@
 # -- Classes and Object Oriented Programming [OOP]
-
-# OOP is a way of organizing code that is based on real-world objects.
-# A Class will have Attribute and Behaviour
-# Class: A blueprint that defines what an object should have and what it can do.
+    # OOP is a way of organizing code that is based on real-world objects.
+    # A Class will have Attribute and Behaviour
+    # Class: A blueprint that defines what an object should have and what it can do.
