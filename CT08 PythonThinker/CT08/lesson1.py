1b22cde678b656d844ef0e1123e1e075b9568622
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-11 03:17:21 +0000

    May 11, 2025, 3:17 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -48,7 +48,7 @@ for i in range(len(egg_code)):
         valid += 1
         # for visuals
         print(f"{EggMethod}{EggCountry}{EggID}")
-        print(LINE)
+print(LINE)
 if valid == len(egg_code):
     print("Codes for the entire batch of eggs are valid.")
     # Collate the number of eggs sampled according to farm method
