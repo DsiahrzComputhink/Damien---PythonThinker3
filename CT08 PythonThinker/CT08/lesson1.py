a6cb8d43eacd9fd6951390be415ac50cf1ee8d93
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-18 03:45:06 +0000

    May 18, 2025, 3:45 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -39,7 +39,7 @@ class style():
 LINE = style.bgray + "------------------------------" + style.RESET
 
 def status(num: int, name: str, variable: int):
-    print(style.bblue + f"[{num}]" + style.RESET, "Hunger:" , self.hunger)
+    print(style.bblue + f"[{num}]" + style.RESET, f"{name}":" , self.hunger)
 
 class Tamagotchi:
     def __init__(self,name : str):
