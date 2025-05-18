91c641a530203ea825c05d72b97813b49c5fdc44
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-18 03:18:17 +0000

    May 18, 2025, 3:18 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -1,3 +1,45 @@
+class style():
+
+    BOLD = '\033[1m'
+    ITALIC = '\033[3m'
+
+    UNDERLINE = '\033[4m'
+    CANCEL = '\033[9m'
+
+    bgbwhite = '\033[7m'
+
+    black = '\033[8m'
+    bgray = '\033[30m'
+    dred = '\033[31m'
+    dgreen = '\033[32m'
+    dyellow = '\033[33m'
+    dblue = '\033[34m'
+    dpurple = '\033[35m'
+    dcyan = '\033[36m'
+    dwhite = '\033[37m'
+
+    bgray = '\033[90m'
+    bred = '\033[91m'
+    bgreen = '\033[92m'
+    byellow = '\033[93m'
+    bblue = '\033[94m'
+    bpurple = '\033[95m'
+    bcyan = '\033[96m'
+    bwhite = '\033[97m'
+
+    RESET = '\033[0m'
+
+    # Generic Colours
+    primary = dblue
+    secondary = bgray
+    
+    warning = dyellow
+    error = dred
+LINE = style.bgray + "------------------------------" + style.RESET
+
+
+print(style.bgray + "Placeholder" + style.RESET)
+
 # -- Classes and Object Oriented Programming [OOP]
     # OOP is a way of organizing code that is based on real-world objects.
     # A Class will have Attribute and Behaviour
