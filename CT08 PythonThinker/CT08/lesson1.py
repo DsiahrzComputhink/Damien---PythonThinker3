82551e3988d212f22a0d867e5795d0b1ab88fffe
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-18 04:08:52 +0000

    May 18, 2025, 4:08 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -76,7 +76,7 @@ class Tamagotchi:
         status(1,"Hunger",self.hunger)
         status(2,"Energy",self.energy)
         status(3,"Happiness",self.happiness)
-        print(style.bblue + f"[4]" + style.RESET, f"{name}:" , self.age," days ")
+        print(style.bblue + f"[4]" + style.RESET, f"Age:" , self.age," days ")
     
     def feed(self):
         randomnum = random.randint(5,15)
