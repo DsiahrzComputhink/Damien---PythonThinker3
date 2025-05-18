ce2a2da1d4aef8dc60f5acd97c200b5f47eb560b
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-18 04:16:10 +0000

    May 18, 2025, 4:16 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -123,7 +123,7 @@ class Tamagotchi:
             print(style.BOLD + f"{name}" + style.RESET,"grew older!")
             print("Age:",style.bgreen + f"{self.age}" + style.RESET,"Days old")
         else:
-            print(f"{name}")
+            print(f"You sucessfully raised up {name}!")
 
 name = "Roger"
 pet = Tamagotchi(name)
