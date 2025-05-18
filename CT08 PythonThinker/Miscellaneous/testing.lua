c403ef09b2ca92031174dd98098ce5088842353f
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-18 02:56:42 +0000

    May 18, 2025, 2:56 AM

--- CT08 PythonThinker/Miscellaneous/testing.lua
+++
@@ -1,8 +0,0 @@
-local part = instance.new("Part")
-part.Parent = workspace
-part.Size = Vector3.new(1, 1, 1)
-part.Position = Vector3.new(0, 0, 0)
-part.Anchored = true
-part.BrickColor = BrickColor.new("Bright red")
-part.Material = Enum.Material.SmoothPlastic
-part.CanCollide = false         
