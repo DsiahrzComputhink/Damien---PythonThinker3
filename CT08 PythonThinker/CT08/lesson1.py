efdd62490182d56d0415435f1f773f1850053b57
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-18 04:19:55 +0000

    May 18, 2025, 4:19 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -123,15 +123,16 @@ class Tamagotchi:
         else:
             if self.energy == 0:
                 print("Your pet died due to",style.bred + "[Exhaustion]" + style.RESET)
-                if self.hunger == 100:
-                    print("Your pet died due to",style.bred + "[Starvation]" + style.RESET)
             else:
-                if self.age < 15:
-                    self.age += 1
-                    print(style.BOLD + f"{name}" + style.RESET,"grew older!")
-                    print("Age:",style.bgreen + f"{self.age}" + style.RESET,"Days old")
+                if self.happiness == 0:
+                    print("Your pet died due to",style.bred + "[Depression]" + style.RESET)
                 else:
-                    print(f"You sucessfully raised up {name}!")
+                    if self.age < 15:
+                        self.age += 1
+                        print(style.BOLD + f"{name}" + style.RESET,"grew older!")
+                        print("Age:",style.bgreen + f"{self.age}" + style.RESET,"Days old")
+                    else:
+                        print(f"You sucessfully raised up {name}!")
 
 name = "Roger"
 pet = Tamagotchi(name)
