8c83858a1926f81f083a7fbc8fa94bbf6471d7a6
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-18 03:57:49 +0000

    May 18, 2025, 3:57 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -65,7 +65,7 @@ class Tamagotchi:
     def feed(self):
         randomnum = random.randint(5,15)
         randomnum2 = random.randint(1,5)
-        randomnum3 = random.randint(1,5)
+        randomnum3 = random.randint(5,10)
         self.hunger = self.hunger - randomnum
         self.energy = self.energy + randomnum2
         self.happiness = self.happiness - randomnum3
@@ -73,7 +73,7 @@ class Tamagotchi:
         print("You fed",style.BOLD + f"{name}" + style.RESET,"!")
         print(style.bcyan + "Hunger" + style.RESET,"went down by",style.bgreen + f"{randomnum}" + style.RESET,"!")
         print(style.bcyan + "Energy" + style.RESET,"went up by",style.bgreen + f"{randomnum2}" + style.RESET,"!")
-        print(style.bcyan + "Happiness" + style.RESET,"went down by",style.bgreen + f"{randomnum3}" + style.RESET,".")
+        print(style.bcyan + "Happiness" + style.RESET,"went down by",style.bred + f"{randomnum3}" + style.RESET,)
 
     def play(self):
         randomnum = random.randint(5,15)
