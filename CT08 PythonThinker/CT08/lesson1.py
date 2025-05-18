a540128cc37ea81d6c7caeab383c2a076e032781
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-18 03:40:49 +0000

    May 18, 2025, 3:40 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -39,8 +39,8 @@ class style():
 LINE = style.bgray + "------------------------------" + style.RESET
 
 def colorstatus(num: int):
-    if num > 100:
-        return style.bblue
+    if num >= 100:
+        return style.bblue + {num} 
 
 class Tamagotchi:
     def __init__(self,name : str):
