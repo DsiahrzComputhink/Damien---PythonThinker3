d7f5a1b7f9b46ec133423b684b6ac6e9aedf53b9
DsiahrzComputhink <damiensiah11@gmail.com>
2025-05-20 00:55:29 +0000

    May 20, 2025, 12:55 AM

--- CT08 PythonThinker/CT08/lesson1.py
+++ CT08 PythonThinker/CT08/lesson1.py
@@ -81,12 +81,12 @@ class Tamagotchi:
         randomnum2 = random.randint(15,20)
         randomnum3 = random.randint(10,15)
         self.hunger = max(self.hunger + randomnum, 100)
-        self.energy = min(self.hunger - randomnum2, 0)
+        self.energy = max(self.hunger - randomnum2, 0)
         self.happiness = max(self.hunger + randomnum3, 100)
         print(LINE)
         print(style.BOLD + f"{name}" + style.RESET,"went to sleep!")
         print(style.bcyan + "Hunger" + style.RESET,"went down by",style.bred + f"{randomnum}" + style.RESET)
-        print(style.bcyan + "Energy" + style.RESET,"went up by",style.bred + f"{randomnum2}" + style.RESET)
+        print(style.bcyan + "Energy" + style.RESET,"went down by",style.bred + f"{randomnum2}" + style.RESET)
         print(style.bcyan + "Happiness" + style.RESET,"went up by",style.bgreen + f"{randomnum3}" + style.RESET)
 
     def sleep(self):
